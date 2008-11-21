8023909c41d61f28791515bf898a2fe7dbd7bb42
taraben_khoiwala <taraben_khoiwala>
2008-11-21 14:27:44 +0000

    Removed the ConceptValueInterface. Not used any more.
    SVN-Revision: 2889

--- WEB-INF/src/edu/wustl/common/query/pvmanager/ConceptValueInteface.java
+++
@@ -1,7 +0,0 @@
-package edu.wustl.common.query.pvmanager;
-
-import edu.common.dynamicextensions.domaininterface.PermissibleValueInterface;
-
-public interface ConceptValueInteface extends PermissibleValueInterface {
-
-}
