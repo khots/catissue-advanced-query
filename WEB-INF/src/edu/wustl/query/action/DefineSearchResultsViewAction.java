42ab723e0d73d78ecc5a5d419544f3b7b0ea3e18
baljeet_dhindhwal <baljeet_dhindhwal>
2008-12-04 14:32:38 +0000

    Changes related to adding containment objects to IQuery
    SVN-Revision: 3215

--- WEB-INF/src/edu/wustl/query/action/DefineSearchResultsViewAction.java
+++ WEB-INF/src/edu/wustl/query/action/DefineSearchResultsViewAction.java
@@ -1,16 +1,23 @@
-
 package edu.wustl.query.action;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.common.querysuite.queryobject.IQuery;
+import edu.wustl.metadata.util.DyExtnObjectCloner;
 import edu.wustl.query.actionForm.CategorySearchForm;
 import edu.wustl.query.util.global.Constants;
+import edu.wustl.query.util.querysuite.IQueryUpdationUtil;
 import edu.wustl.query.util.querysuite.QueryModuleUtil;
 
 /**
@@ -37,6 +44,24 @@ public class DefineSearchResultsViewAction extends Action
 		request.setAttribute(Constants.CURRENT_PAGE, Constants.DEFINE_RESULTS_VIEW);
 		CategorySearchForm searchForm = (CategorySearchForm) form;
 		searchForm = QueryModuleUtil.setDefaultSelections(searchForm);
+		
+		//By Baljeet ...for containment objects
+		HttpSession session = request.getSession();
+		IQuery query = (IQuery) session.getAttribute(Constants.QUERY_OBJECT);
+		
+	    if(query != null)
+	    {
+	    	IQuery queryClone = new DyExtnObjectCloner().clone(query);
+			Map <EntityInterface, List<EntityInterface>>containedObjectsMap = IQueryUpdationUtil.getAllConatainmentObjects(query,session);
+			
+			//Update the IQuery with containment objects......add onlt those containment objects which are not present in IQuery
+			IQueryUpdationUtil.addConatinmentObjectsToIquery(query,session,queryClone);
+			
+			//Add the link/association among parent and containment entities
+			IQueryUpdationUtil.addLinks(containedObjectsMap, session,queryClone);
+	    }
+				
+		//ends
 		return mapping.findForward(Constants.SUCCESS);
 	}
 }
