a7b42e7ee080f72d6da0906195d4abd7c53cdad4
vijay_pande <vijay_pande>
2008-12-04 06:43:50 +0000

    action class for workflow
    SVN-Revision: 3150

--- WEB-INF/src/edu/wustl/query/action/WorkflowAction.java
+++ WEB-INF/src/edu/wustl/query/action/WorkflowAction.java
@@ -2,19 +2,18 @@ package edu.wustl.query.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import edu.wustl.common.beans.SessionDataBean;
-import edu.wustl.common.util.global.Constants;
 import edu.wustl.query.actionForm.WorkflowForm;
 import edu.wustl.query.bizlogic.BizLogicFactory;
 import edu.wustl.query.bizlogic.WorkflowBizLogic;
 import edu.wustl.query.domain.Workflow;
+import edu.wustl.query.util.global.Constants;
+import edu.wustl.query.util.global.Utility;
 
 /**
  * 
@@ -31,33 +30,15 @@ public class WorkflowAction extends Action
 			HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
 		System.out.println("In Workflow Action");
-		HttpSession session = request.getSession();
 		final WorkflowForm workflowForm = (WorkflowForm) form;
 		
 		Workflow workflow = new Workflow();
 		workflow.setName(workflowForm.getName());
 		
-		WorkflowBizLogic bizLogic = (WorkflowBizLogic)BizLogicFactory.getInstance().getBizLogic(edu.wustl.query.util.global.Constants.WORKFLOW_BIZLOGIC_ID);
-		bizLogic.insert(workflow, getSessionData(request), Constants.HIBERNATE_DAO);
+		WorkflowBizLogic bizLogic = (WorkflowBizLogic)BizLogicFactory.getInstance().getBizLogic(Constants.WORKFLOW_BIZLOGIC_ID);
+		bizLogic.insert(workflow, Utility.getSessionData(request), Constants.HIBERNATE_DAO);
 		
 		return mapping.findForward(Constants.SUCCESS);
-	}
-	
-	
-	public SessionDataBean getSessionData(HttpServletRequest request) 
-    {
-		Object obj = request.getSession().getAttribute(Constants.SESSION_DATA);
-		 
-		if(obj == null)
-		{
-			obj = request.getSession().getAttribute(Constants.TEMP_SESSION_DATA);
-		}
-		if(obj!=null)
-		{
-			SessionDataBean sessionData = (SessionDataBean) obj;
-			return  sessionData;
-		} 
-		return null;
 	}
 }
 
