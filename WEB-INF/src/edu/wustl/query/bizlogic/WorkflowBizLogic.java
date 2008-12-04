4e0987641023aec0ab47946a710760576c3886da
vijay_pande <vijay_pande>
2008-12-04 06:44:20 +0000

    bizlogic for workflow
    SVN-Revision: 3152

--- WEB-INF/src/edu/wustl/query/bizlogic/WorkflowBizLogic.java
+++ WEB-INF/src/edu/wustl/query/bizlogic/WorkflowBizLogic.java
@@ -5,22 +5,33 @@ import edu.wustl.common.bizlogic.DefaultBizLogic;
 import edu.wustl.common.dao.DAO;
 import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
 import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.logger.Logger;
 import edu.wustl.query.domain.Workflow;
 
+
 /**
- * 
- * @author ravindra_jain
- *
+ * @author vijay_pande
+ * BizLogic class to insert/update WorkFlow Object
  */
 public class WorkflowBizLogic extends DefaultBizLogic
 {
+	private static org.apache.log4j.Logger logger =Logger.getLogger(WorkflowBizLogic.class);
 	
 	protected void insert(Object obj, DAO dao, SessionDataBean sessionDataBean) throws DAOException
 	{
 		Workflow workflow = (Workflow) obj;
 		
-		System.out.println("In  WORKFLOW  BIZ LOGIC >>>>>> INSERT METHOD");
-		System.out.println("#### Workflow Name #### ::  "+workflow.getName());
+		logger.info("In  WORKFLOW  BIZ LOGIC >>>>>> INSERT METHOD");
+		logger.info("#### Workflow Name #### ::  "+workflow.getName());
+		try
+		{
+			dao.insert(workflow, sessionDataBean, false, false);
+		}
+		catch (UserNotAuthorizedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
