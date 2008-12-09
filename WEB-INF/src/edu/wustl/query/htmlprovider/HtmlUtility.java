4986be52783d338ea75c2e50036435794131d29c
rukhsana_sameer <rukhsana_sameer>
2008-12-09 08:09:17 +0000

    removing unused imports
    SVN-Revision: 3307

--- WEB-INF/src/edu/wustl/query/htmlprovider/HtmlUtility.java
+++ WEB-INF/src/edu/wustl/query/htmlprovider/HtmlUtility.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import edu.common.dynamicextensions.domain.PermissibleValue;
 import edu.common.dynamicextensions.domain.UserDefinedDE;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
