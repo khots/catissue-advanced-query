9dd0708976f28e8532e46065bf8b7b473a302447
taraben_khoiwala <taraben_khoiwala>
2008-12-09 08:30:58 +0000

    MedLookUpManager shifted to pvmanager package.
    SVN-Revision: 3311

---
+++ WEB-INF/src/edu/wustl/common/query/pvmanager/impl/MedLookUpManager.java
@@ -0,0 +1,83 @@
+package edu.wustl.common.query.pvmanager.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wustl.common.dao.DAOFactory;
+import edu.wustl.common.dao.JDBCDAO;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.query.util.global.Constants;
+
+public class MedLookUpManager
+{
+	Map<String,List<String> > pvMap = null;
+	private static MedLookUpManager medLookUpManager = null;
+	
+	private MedLookUpManager()
+	{
+		
+	}
+	
+	public static MedLookUpManager instance()
+	{
+		if(medLookUpManager == null)
+		{
+			medLookUpManager = new MedLookUpManager();
+			medLookUpManager.init();
+		}
+		
+		return medLookUpManager;
+		
+	}
+	
+	private void init()
+	{
+		JDBCDAO dao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
+		try
+		{
+			dao.openSession(null);
+			List<List<String>> dataList = dao.executeQuery("select synonym,id from MED_LOOKUP_TABLE", null, false, false, null);
+			if(!dataList.isEmpty())
+			{
+				pvMap = new HashMap<String, List<String>>();
+				String pvFilter;
+				for(int i=0; i < dataList.size(); i++)
+				{
+					pvFilter = dataList.get(i).get(0).substring(0,dataList.get(i).get(0).indexOf("^") + 1) + "%";
+					if(pvMap.containsKey(pvFilter))
+					{
+						pvMap.get(pvFilter).add(dataList.get(i).get(1));
+					}
+					else
+					{
+						List<String> pvList = new ArrayList<String>();
+						pvList.add(dataList.get(i).get(1));
+						pvMap.put(pvFilter,pvList);
+					}
+				}
+			}
+		}
+		catch (DAOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public List<String> getPermissibleValues(String pvFilter)
+	{
+		if(pvMap != null)
+			return pvMap.get(pvFilter);
+		else
+			return null;
+
+	}
+}
