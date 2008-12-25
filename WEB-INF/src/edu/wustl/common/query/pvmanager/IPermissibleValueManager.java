3102bf13c354fdee90849c35e33efcd23b460a28
taraben_khoiwala <taraben_khoiwala>
2008-12-25 17:45:54 +0000

    Added Exception handler.
    SVN-Revision: 3904

--- WEB-INF/src/edu/wustl/common/query/pvmanager/IPermissibleValueManager.java
+++ WEB-INF/src/edu/wustl/common/query/pvmanager/IPermissibleValueManager.java
@@ -5,10 +5,11 @@ import java.util.List;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.common.dynamicextensions.domaininterface.PermissibleValueInterface;
+import edu.wustl.common.query.pvmanager.impl.PVManagerException;
 
 public interface IPermissibleValueManager
 {
-	public List<PermissibleValueInterface> getPermissibleValueList(AttributeInterface attribute,EntityInterface entity);
-	public boolean showListBoxForPV(AttributeInterface attribute,EntityInterface entity);
+	public List<PermissibleValueInterface> getPermissibleValueList(AttributeInterface attribute,EntityInterface entity) throws PVManagerException;
+	public boolean showListBoxForPV(AttributeInterface attribute,EntityInterface entity) throws PVManagerException;
 	public boolean isEnumerated(AttributeInterface attribute,EntityInterface entity);
 }
