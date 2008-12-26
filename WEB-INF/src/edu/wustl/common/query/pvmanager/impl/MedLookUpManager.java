3bcbccbeca59418a9ed4bcee1008af65f82f7137
taraben_khoiwala <taraben_khoiwala>
2008-12-26 05:20:43 +0000

    Commented the init and getPermissibleVlaues methods.Added new getPermissibleValues and getQuery method.
    SVN-Revision: 3912

--- WEB-INF/src/edu/wustl/common/query/pvmanager/impl/MedLookUpManager.java
+++ WEB-INF/src/edu/wustl/common/query/pvmanager/impl/MedLookUpManager.java
@@ -1,20 +1,20 @@
 package edu.wustl.common.query.pvmanager.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.JDBCDAO;
 import edu.wustl.common.util.dbManager.DAOException;
-import edu.wustl.common.util.logger.Logger;
 import edu.wustl.query.util.global.Constants;
+import edu.wustl.query.util.global.Variables;
 
 public class MedLookUpManager
 {
 	Map<String,List<String> > pvMap = null;
 	private static MedLookUpManager medLookUpManager = null;
+	private String lookUpTable=null;
 	
 	private MedLookUpManager()
 	{
@@ -26,14 +26,15 @@ public class MedLookUpManager
 		if(medLookUpManager == null)
 		{
 			medLookUpManager = new MedLookUpManager();
-			medLookUpManager.init();
+			medLookUpManager.lookUpTable = Variables.properties.getProperty("med.lookup.table");
+			//medLookUpManager.init();
 		}
 		
 		return medLookUpManager;
 		
 	}
 	
-	private void init()
+	/*private void init()
 	{
 		JDBCDAO dao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
 		try
@@ -63,26 +64,84 @@ public class MedLookUpManager
 		catch (DAOException e)
 		{
 			// TODO Auto-generated catch block
-			Logger.out.error(e.getMessage(),e);
+			e.printStackTrace();
 		}
 		catch (ClassNotFoundException e)
 		{
 			// TODO Auto-generated catch block
-			Logger.out.error(e.getMessage(),e);
+			e.printStackTrace();
 		}
 		
-	}
+	}*/
 	
-	public List<String> getPermissibleValues(String pvFilter)
+	/*public List<String> getPermissibleValues(String pvFilter)
 	{
 		if(pvMap != null)
-		{
 			return pvMap.get(pvFilter);
-		}
 		else
-		{
 			return null;
+	}*/
+	public List<String> getPermissibleValues(List<String> pvFilter) throws PVManagerException 
+	{
+		List<String> pvList = null;
+		JDBCDAO dao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
+		try
+		{
+			dao.openSession(null);
+			String query = getQuery(pvFilter);
+			List<List<String>> dataList = dao.executeQuery(query, null, false, false, null);
+			if(!dataList.isEmpty())
+			{
+				pvList = new ArrayList<String>();
+			
+				for(int i=0; i < dataList.size(); i++)
+				{
+					pvList.add(dataList.get(i).get(1));
+				}
+			}
 		}
+		catch (DAOException e)
+		{
+			
+			throw new PVManagerException("Error occured while fetching the permissible concept codes from MED.",e);
+			
+		}
+		catch (ClassNotFoundException e)
+		{
+			throw new PVManagerException("Error occured while fetching the permissible concept codes from MED.",e);			
+		}	
+		finally
+		{
+			try
+			{
+				dao.closeSession();
+			}
+			catch (DAOException e)
+			{
+				throw new PVManagerException("Error occured while fetching the permissible concept codes from MED.",e);
+			}
+		}
+		
+		return pvList;
 
+	}
+	
+	private String getQuery(List<String> pvFilterList)
+	{
+		StringBuffer query = new StringBuffer("select synonym,id from ");
+		query.append(lookUpTable);
+		query.append(" where ");
+		int indx=0;
+		for(; indx<pvFilterList.size()-1;indx++)
+		{
+			query.append("synonym like '");
+			query.append(pvFilterList.get(indx));
+			query.append("' or ");
+		}
+		query.append("synonym like '");
+		query.append(pvFilterList.get(indx));
+		query.append("'");
+		
+		return query.toString();
 	}
 }
