bea7eeafa72c0905587c3e4ebe6bd0b0d9e448fb
taraben_khoiwala <taraben_khoiwala>
2008-12-26 05:21:12 +0000

    Exception class for PermissibleValueManagers.
    SVN-Revision: 3913

---
+++ WEB-INF/src/edu/wustl/common/query/pvmanager/impl/PVManagerException.java
@@ -0,0 +1,34 @@
+package edu.wustl.common.query.pvmanager.impl;
+
+public class PVManagerException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7965145520733463765L;
+
+	public PVManagerException()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public PVManagerException(String arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PVManagerException(Throwable arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PVManagerException(String arg0, Throwable arg1)
+	{
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
