30ee9f2ae0e7e0e6cb56787b30d95a7b67ade4c5
niharika_sharma <niharika_sharma>
2009-01-12 12:54:04 +0000

    Added for Query Design Changes.
    SVN-Revision: 4284

---
+++ WEB-INF/src/edu/wustl/query/util/querysuite/QueryUIManager.java
@@ -0,0 +1,77 @@
+package edu.wustl.query.util.querysuite;
+
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.query.factory.AbstractQueryFactory;
+import edu.wustl.common.query.factory.AbstractQueryManagerFactory;
+import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
+import edu.wustl.common.querysuite.exceptions.SqlException;
+import edu.wustl.common.querysuite.queryobject.IQuery;
+import edu.wustl.query.querymanager.AbstractQueryManager;
+import edu.wustl.query.util.global.Constants;
+import edu.wustl.common.query.AbstractQuery;
+
+/*
+ *   
+ */
+public class QueryUIManager extends AbstractQueryUIManager{
+
+	
+	private AbstractQuery abstractQuery;
+	
+
+	public QueryUIManager() {
+	}
+
+	QueryDetails queryDetailsObj;
+
+	/**
+	 * @param request
+	 * @param query
+	 */
+	public QueryUIManager(HttpServletRequest request, IQuery query) {
+
+		this.request = request;
+		this.session = request.getSession();
+		SessionDataBean sessionDataBean = ((SessionDataBean) session
+				.getAttribute(Constants.SESSION_DATA));
+		long user_id = sessionDataBean.getUserId();
+		this.abstractQuery = AbstractQueryFactory.getDefaultAbstractQuery();
+		this.abstractQuery.setQuery(query);
+		this.abstractQuery.setUser_id(user_id);
+		isSavedQuery = Boolean.valueOf((String) session
+				.getAttribute(Constants.IS_SAVED_QUERY));
+		queryDetailsObj = new QueryDetails(session);
+	}
+
+
+
+	/**
+	 * @throws QueryModuleException
+	 */
+	public void processQuery() throws QueryModuleException {
+		AbstractQueryManager queryManager = AbstractQueryManagerFactory.getDefaultAbstractQueryManager();
+		QueryModuleException queryModExp;
+		try {
+			 queryManager.execute(abstractQuery);
+
+		} catch (MultipleRootsException e) {
+			queryModExp = new QueryModuleException(e.getMessage(),
+					QueryModuleError.MULTIPLE_ROOT);
+			throw queryModExp;
+		} catch (SqlException e) {
+			queryModExp = new QueryModuleException(e.getMessage(),
+					QueryModuleError.SQL_EXCEPTION);
+			throw queryModExp;
+		} finally {
+		}
+		Map<EntityInterface, List<EntityInterface>> mainEntityMap = QueryCSMUtil
+				.setMainObjectErrorMessage(abstractQuery.getQuery(), request
+						.getSession(), queryDetailsObj);
+		queryDetailsObj.setMainEntityMap(mainEntityMap);
+	}
+}
