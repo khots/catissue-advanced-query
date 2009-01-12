fd15a169b400f38745e01de468b40b2af3c4dda2
ravindra_jain <ravindra_jain>
2009-01-12 16:07:07 +0000

    changes as per new CIDER Query Design
    SVN-Revision: 4293

--- WEB-INF/src/edu/wustl/query/querymanager/CiderQueryManager.java
+++ WEB-INF/src/edu/wustl/query/querymanager/CiderQueryManager.java
@@ -1,19 +1,19 @@
 /**
  * 
  */
+
 package edu.wustl.query.querymanager;
 
 import java.sql.SQLException;
 
 import edu.wustl.common.query.CiderQuery;
 import edu.wustl.common.query.factory.QueryGeneratorFactory;
+import edu.wustl.common.query.itablemanager.CIDERITableManager;
 import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
 import edu.wustl.common.querysuite.exceptions.SqlException;
 import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.query.domain.Workflow;
 import edu.wustl.query.queryengine.impl.IQueryGenerator;
-import edu.wustl.query.util.querysuite.QueryModuleError;
-import edu.wustl.query.util.querysuite.QueryModuleException;
 import edu.wustl.query.ExecutionManager.CIDERQueryExecutionThread;
 import edu.wustl.query.ExecutionManager.QueryExecutionThread;
 
@@ -21,13 +21,15 @@ import edu.wustl.query.ExecutionManager.QueryExecutionThread;
  * @author supriya_dankh
  * 
  */
-public class CiderQueryManager extends AbstractQueryManager {
+public class CiderQueryManager extends AbstractQueryManager
+{
 
 	/**
 	 * @param workflow
 	 * @return
 	 */
-	public int execute(Workflow workflow) {
+	public int execute(Workflow workflow)
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -35,17 +37,23 @@ public class CiderQueryManager extends AbstractQueryManager {
 	/**
 	 * @param query_excecution_id
 	 * @return
+	 * @throws DAOException 
+	 * @throws SQLException 
 	 */
-	public Count getQueryCount(int query_excecution_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Count getQueryCount(int query_excecution_id) throws DAOException, SQLException
+	{
+		Count count = null;
+		CIDERITableManager manager = CIDERITableManager.getInstance();
+			count = manager.getCount(query_excecution_id);
+		return count;
 	}
 
 	/**
 	 * @param query_excecution_id
 	 * @return
 	 */
-	public Count getWorkflowCount(int query_excecution_id) {
+	public Count getWorkflowCount(int query_excecution_id)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -56,7 +64,8 @@ public class CiderQueryManager extends AbstractQueryManager {
 	 * 
 	 * @param queryExecId
 	 */
-	public void cancel(int query_execution_id) {
+	public void cancel(int query_execution_id)
+	{
 		// LOGIC TO UPDATE STATUS OF THREAD in iTABLE LOG
 		// Stop Thread corresponding to queryExecId
 
@@ -66,42 +75,52 @@ public class CiderQueryManager extends AbstractQueryManager {
 
 	/**
 	 * 
-	 * @param query
-	 *            SQL query
-	 * @return Query Execution Id
+	 * @param ciderQueryObj
+	 * @return
+	 * @throws MultipleRootsException
+	 * @throws SQLException
+	 * @throws DAOException
+	 * @throws SqlException
 	 */
-	public int execute(CiderQuery ciderQueryObj) throws MultipleRootsException ,SqlException{
+	public int execute(CiderQuery ciderQueryObj) throws MultipleRootsException, SqlException
+	{
+		// String xQuery = "select personUpi_1 Column0 ,id_1 Column1 ,dateOfDeath_2 Column2 ,dateOfBirth_2 Column3 ,id_2 Column4 ,effectiveEndTimeStamp_2 Column5 ,effectiveStartTimeStamp_2 Column6 ,mothersMaidenName_2 Column7 ,placeOfBirth_2 Column8 ,socialSecurityNumber_2 Column9 from xmltable(' for $Person_1 in db2-fn:xmlcolumn(\"DEMOGRAPHICS.XMLDATA\")/Person ,$Demographics_2 in $Person_1/demographicsCollection/demographics where (exists($Person_1/personUpi)) and($Demographics_2/dateOfBirth>xs:dateTime(\"1920-10-10T23:59:59\") )  return <return><Person_1>{$Person_1}</Person_1><Demographics_2>{$Demographics_2}</Demographics_2></return>' columns personUpi_1 varchar(1000) path 'Person_1/Person/personUpi' ,id_1 DECIMAL(31) path 'Person_1/Person/id' ,dateOfDeath_2 TimeStamp path 'Demographics_2/demographics/dateOfDeath' ,dateOfBirth_2 TimeStamp path 'Demographics_2/demographics/dateOfBirth' ,id_2 DECIMAL(31) path 'Demographics_2/demographics/id' ,effectiveEndTimeStamp_2 TimeStamp path 'Demographics_2/demographics/effectiveEndTimeStamp' ,effectiveStartTimeStamp_2 TimeStamp path 'Demographics_2/demographics/effectiveStartTimeStamp' ,mothersMaidenName_2 varchar(1000) path 'Demographics_2/demographics/mothersMaidenName' ,placeOfBirth_2 varchar(1000) path 'Demographics_2/demographics/placeOfBirth' ,socialSecurityNumber_2 varchar(1000) path 'Demographics_2/demographics/socialSecurityNumber')";
+
 		int queryExecId = -1;
-		try {
+		try
+		{
 			IQueryGenerator queryGenerator = QueryGeneratorFactory
 					.getDefaultQueryGenerator();
 			String generatedQuery = queryGenerator.generateQuery(ciderQueryObj
 					.getQuery());
+
 			Thread thread = null;
 			QueryExecutionThread queryExecutionThread = null;
 
-			queryExecId = CIDERQueryExecutionThread
-					.getQueryExecutionId(ciderQueryObj);
+			queryExecId = CIDERQueryExecutionThread.getQueryExecutionId(ciderQueryObj);
+			ciderQueryObj.setQueryExecId(queryExecId);
+			ciderQueryObj.setQueryString(generatedQuery);
 
-			/*
-			 * LOGIC to Insert new entry into the QUERY_EXECUTION_LOG table
-			 */
-
-			// queryExecId = getQueryExecutionId();
 			thread = new Thread(new CIDERQueryExecutionThread(ciderQueryObj));
 
-			thread.start();
-
 			queryThreads.put(queryExecId, queryExecutionThread);
-		} catch (MultipleRootsException e) {
+
+			thread.start();
+		}
+		catch (DAOException e)
+		{
 			e.printStackTrace();
-		} catch (SqlException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
-		} catch (DAOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
+		}
+		catch (SqlException e)
+		{
+			throw e;
+		}
+		finally
+		{
 			// close connection
 			// close result set
 
