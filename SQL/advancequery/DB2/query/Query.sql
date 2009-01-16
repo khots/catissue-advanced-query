3ed8af4c23b4408da8fda8e07d86b683714ff01a
pavan_kalantri <pavan_kalantri>
2009-01-16 17:13:46 +0000

    latest sql file
    SVN-Revision: 4403

--- SQL/advancequery/DB2/query/Query.sql
+++ SQL/advancequery/DB2/query/Query.sql
@@ -22,8 +22,22 @@ where abstract_entity_id =
 (select identifier from dyextn_abstract_metadata
 where name = 'Encounter'));
 
-update dyextn_constraint_properties 
-set target_entity_key = 'personUpi'
+update dyextn_database_properties
+set name = 'MEDICATIONS'
+where identifier =
+(select identifier from dyextn_table_properties
+where abstract_entity_id =
+(select identifier from dyextn_abstract_metadata
+where name = 'MedicationOrder'));
+
+update dyextn_database_properties
+set name = 'personUpi'
+where identifier =
+(select identifier from dyextn_column_properties
+where CNSTR_KEY_PROP_ID=
+(select identifier from DYEXTN_CONSTRAINTKEY_PROP
+where TGT_CONSTRAINT_KEY_ID=
+(select identifier from dyextn_constraint_properties
 where association_id = 
 (select identifier from dyextn_association
 where identifier in
@@ -33,11 +47,17 @@ where entiy_id in
 where name = 'Person'))
 and target_entity_id =
 (select identifier from dyextn_abstract_metadata
-where name = 'LaboratoryProcedure'));
+where name = 'LaboratoryProcedure')))));
 
 
-update dyextn_constraint_properties 
-set target_entity_key = 'personUpi'
+update dyextn_database_properties
+set name = 'personUpi'
+where identifier =
+(select identifier from dyextn_column_properties
+where CNSTR_KEY_PROP_ID=
+(select identifier from DYEXTN_CONSTRAINTKEY_PROP
+where TGT_CONSTRAINT_KEY_ID=
+(select identifier from dyextn_constraint_properties
 where association_id = 
 (select identifier from dyextn_association
 where identifier in
@@ -47,4 +67,23 @@ where entiy_id in
 where name = 'Person'))
 and target_entity_id =
 (select identifier from dyextn_abstract_metadata
-where name = 'Encounter'));
+where name = 'Encounter')))));
+
+update dyextn_database_properties
+set name = 'personUpi'
+where identifier =
+(select identifier from dyextn_column_properties
+where CNSTR_KEY_PROP_ID=
+(select identifier from DYEXTN_CONSTRAINTKEY_PROP
+where TGT_CONSTRAINT_KEY_ID=
+(select identifier from dyextn_constraint_properties
+where association_id = 
+(select identifier from dyextn_association
+where identifier in
+(select identifier from dyextn_attribute
+where entiy_id in  
+(select identifier from dyextn_abstract_metadata
+where name = 'Person'))
+and target_entity_id =
+(select identifier from dyextn_abstract_metadata
+where name = 'MedicationOrder')))));
