deeeb4f60b5b7c989a5817b2d41ba27903e3d688
chitra_garg <chitra_garg>
2009-01-22 05:18:13 +0000

    comments added
    SVN-Revision: 4503

--- WEB-INF/src/edu/wustl/common/beans/RecentQueriesBean.java
+++ WEB-INF/src/edu/wustl/common/beans/RecentQueriesBean.java
@@ -1,63 +1,115 @@
 package edu.wustl.common.beans;
-
-
+/**
+ * @author chitra_garg
+ *used as bean for setting the 
+ *parameters needed in recent query ajax call 
+ */
 public class RecentQueriesBean
 {
+	
+	/**
+	 * title for query
+	 */
 	String queryTitle;
+	
+	/**
+	 * status that is in-progress or Completed 
+	 */
 	String status;
+	
+	/**
+	 * count
+	 */
 	Long resultCount;
+	
+	/**
+	 * creation date assigned to query
+	 */
 	String queryCreationDate;
+	/**
+	 * xecution id assigned to query
+	 */
 	Long queyExecutionId;
 	
+	/**
+	 * @return query title
+	 */
 	public String getQueryTitle()
 	{
 		return queryTitle;
 	}
 	
+	/**
+	 * @param queryTitle = title for query
+	 */
 	public void setQueryTitle(String queryTitle)
 	{
 		this.queryTitle = queryTitle;
 	}
 	
+	/**
+	 * @return status that is in-progress or Completed 
+	 */
 	public String getStatus()
 	{
 		return status;
 	}
 	
+	/**
+	 * @param status set that is in-progress or Completed 
+	 */
 	public void setStatus(String status)
 	{
 		this.status = status;
 	}
 	
+	/**
+	 * @return count 
+	 */ 
 	public Long getResultCount()
 	{
 		return resultCount;
 	}
 	
+	/**
+	 * @param resultCount=count
+	 */
 	public void setResultCount(Long resultCount)
 	{
 		this.resultCount = resultCount;
 	}
 
 	
+	/**
+	 * @return creation date assigned to query
+	 */
 	public String getQueryCreationDate()
 	{
 		return queryCreationDate;
 	}
 
 	
+	/**
+	 * @param queryCreationDate =creation date assigned to query
+	 */
 	public void setQueryCreationDate(String queryCreationDate)
 	{
 		this.queryCreationDate = queryCreationDate;
 	}
 
 	
+	/**
+	 * @return execution id assigned to query
+	 */
 	public Long getQueyExecutionId()
 	{
 		return queyExecutionId;
 	}
 
 	
+	/**
+	 * @param queyExecutionId=execution id assigned to query
+	 */
 	public void setQueyExecutionId(Long queyExecutionId)
 	{
 		this.queyExecutionId = queyExecutionId;
--- WEB-INF/src/edu/wustl/query/action/RecentQueriesAjaxHandlerAction.java
+++ WEB-INF/src/edu/wustl/query/action/RecentQueriesAjaxHandlerAction.java
@@ -1,3 +1,4 @@
+
 package edu.wustl.query.action;
 
 import java.io.Writer;
@@ -15,58 +16,67 @@ import org.json.JSONObject;
 import edu.wustl.common.query.factory.AbstractQueryUIManagerFactory;
 import edu.wustl.common.util.logger.Logger;
 import edu.wustl.query.querymanager.Count;
+import edu.wustl.query.util.global.Constants;
 import edu.wustl.query.util.querysuite.AbstractQueryUIManager;
 import edu.wustl.cider.util.CiderQueryUIManager;
 import edu.wustl.query.util.querysuite.QueryModuleException;
 
-
+/**
+ * @author chitra_garg
+ *handles ajax call for recent queries
+ */
 public class RecentQueriesAjaxHandlerAction extends Action
 {
+	private static org.apache.log4j.Logger logger =Logger.getLogger(RecentQueriesAjaxHandlerAction.class);
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
 		Writer writer = response.getWriter();
-		int queryExecutionId=Integer.valueOf(request.getParameter("executionLogId"));
-		String index =request.getParameter("index");
-
+		int queryExecutionId = Integer.valueOf(request.getParameter(Constants.EXECUTION_LOG_ID));
+		String index = request.getParameter(Constants.INDEX);
 		Count countObject = populateCountObject(queryExecutionId);
-
 		//create json object by count object adding Query status, count and execution id
-		JSONObject resultObject = createResultJSON(countObject,index);
-		response.setContentType("text/xml");
-		writer.write(new JSONObject().put("resultObject", resultObject).toString());
-		
+		JSONObject resultObject = createResultJSON(countObject, index);
+		response.setContentType(Constants.CONTENT_TYPE_TEXT);
+		writer.write(new JSONObject().put(Constants.RESULT_OBJECT, resultObject).toString());
+
 		return null;
-		
+
 	}
 
+	/**
+	 * 
+	 * @param queryExecutionId= query execution id
+	 * @return count object for given execution id
+	 * @throws QueryModuleException
+	 */
 	private Count populateCountObject(int queryExecutionId) throws QueryModuleException
 	{
 		//retrieve count with query execution id
-		AbstractQueryUIManager qUIManager = AbstractQueryUIManagerFactory.getDefaultAbstractUIQueryManager();
-		Count countObject =((CiderQueryUIManager)qUIManager).getCount(queryExecutionId);
+		AbstractQueryUIManager qUIManager = AbstractQueryUIManagerFactory
+				.getDefaultAbstractUIQueryManager();
+		Count countObject = ((CiderQueryUIManager) qUIManager).getCount(queryExecutionId);
 		return countObject;
 	}
-	
+
 	/**
 	 * Creates a json object and appends query count, status, execution id to it.
 	 * @param countObject
 	 * @return
 	 */
-	@SuppressWarnings("deprecation")
-	private JSONObject createResultJSON(Count countObject,String index) 
+	private JSONObject createResultJSON(Count countObject, String index)
 	{
 		JSONObject resultObject = new JSONObject();
 		try
 		{
-			resultObject.append("queryCount",countObject.getCount());
-			resultObject.append("status", countObject.getStatus());
-			resultObject.append("executionId", countObject.getQuery_exection_id());
-			resultObject.append("index", index);
+			resultObject.append(Constants.QUERY_COUNT, countObject.getCount());
+			resultObject.append(Constants.GET_COUNT_STATUS, countObject.getStatus());
+			resultObject.append(Constants.EXECUTION_ID, countObject.getQuery_exection_id());
+			resultObject.append(Constants.INDEX, index);
 		}
 		catch (JSONException e)
 		{
-			Logger.out.info("error in initializing json object " + e);
+			logger.error("error in initializing json object " + e);
 		}
 		return resultObject;
 	}
