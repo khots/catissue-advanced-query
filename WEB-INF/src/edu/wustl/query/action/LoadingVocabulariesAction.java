99ccbb963eecf608208ede66e976e832db7666c1
amit_doshi <amit_doshi>
2009-01-23 07:14:12 +0000

    Added New Action class for VI Loading Message
    SVN-Revision: 4561

---
+++ WEB-INF/src/edu/wustl/query/action/LoadingVocabulariesAction.java
@@ -0,0 +1,31 @@
+package edu.wustl.query.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+
+public class LoadingVocabulariesAction extends Action
+{
+
+	/**
+	 * This method Will show the loading message on the UI for VI
+	 * @param mapping mapping
+	 * @param form form
+	 * @param request request
+	 * @param response response
+	 * @throws Exception Exception
+	 * @return ActionForward actionForward
+	 */
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		return mapping.findForward("success");
+		
+	}
+}
