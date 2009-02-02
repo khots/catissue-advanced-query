5952028dc638036fd0936bf9dadb065a697112c1
ravindra_jain <ravindra_jain>
2009-02-02 15:23:54 +0000

    to run XQuery test cases
    SVN-Revision: 4698

---
+++ WEB-INF/test/edu/wustl/query/bizlogic/QueryTestAll.java
@@ -0,0 +1,38 @@
+package edu.wustl.query.bizlogic;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class QueryTestAll extends TestSuite
+{
+	/**
+	 * @param args arg
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			junit.awtui.TestRunner.run(QueryTestAll.class);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return test
+	 */
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite("Test suite for QUERY business logic");
+		
+		// For testing FAMEWORK for Query testing
+		// suite.addTestSuite(XQueryGeneratorTestCase.class);
+		
+		// For testing ASynchronous Queries
+		suite.addTestSuite(ASynchronousQueriesTestCases.class);
+		return suite;
+	}
+}
