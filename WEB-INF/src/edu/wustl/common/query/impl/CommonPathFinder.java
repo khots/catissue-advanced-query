ae9ced4e546b94e0887b7228fb3248d37c1c3697
ravindra_jain <ravindra_jain>
2009-02-11 11:30:56 +0000

    added so that test cases can run
    SVN-Revision: 4893

--- WEB-INF/src/edu/wustl/common/query/impl/CommonPathFinder.java
+++ WEB-INF/src/edu/wustl/common/query/impl/CommonPathFinder.java
@@ -14,11 +14,15 @@ import javax.sql.DataSource;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.wustl.cab2b.client.ui.query.IPathFinder;
 import edu.wustl.cab2b.server.path.PathFinder;
+import edu.wustl.common.dao.DatabaseConnectionParams;
 import edu.wustl.common.querysuite.metadata.associations.IInterModelAssociation;
 import edu.wustl.common.querysuite.metadata.associations.IIntraModelAssociation;
 import edu.wustl.common.querysuite.metadata.path.ICuratedPath;
 import edu.wustl.common.querysuite.metadata.path.IPath;
+import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.logger.Logger;
+import edu.wustl.query.util.global.Constants;
+import edu.wustl.query.util.global.Variables;
 
 /**
  * This class is used to find all the possible paths between two entities.
@@ -36,18 +40,27 @@ public class CommonPathFinder implements IPathFinder
 		if (pathFinder == null)
 		{
 			Connection connection = null;
+			DatabaseConnectionParams DB_CONNECTION_PARAMS = new DatabaseConnectionParams();
 			try
 			{
-				InitialContext initialContext = new InitialContext();
-				Context env = (Context) initialContext.lookup("java:comp/env");
-                String dsName = (String) env.lookup("DataSource");
-                Logger.out.info("Data source name found: " + dsName);
-                
-                DataSource dataSource = (DataSource)initialContext.lookup(dsName);
-                Logger.out.info("Data source found: " + dataSource);
-                
-				connection = dataSource.getConnection();
-				Logger.out.info("Connection established: " + connection);
+				if(Variables.isExecutingTestCase)
+				{
+					DB_CONNECTION_PARAMS.openSession(Constants.JNDI_NAME_CIDER);
+					connection = DB_CONNECTION_PARAMS.getConnection();
+				}
+				else
+				{
+					InitialContext initialContext = new InitialContext();
+					Context env = (Context) initialContext.lookup("java:comp/env");
+	                String dsName = (String) env.lookup("DataSource");
+	                Logger.out.info("Data source name found: " + dsName);
+	                
+	                DataSource dataSource = (DataSource)initialContext.lookup(dsName);
+	                Logger.out.info("Data source found: " + dataSource);
+	                
+					connection = dataSource.getConnection();
+					Logger.out.info("Connection established: " + connection);
+				}
 				
 				pathFinder = PathFinder.getInstance(connection);
 			}
@@ -61,16 +74,31 @@ public class CommonPathFinder implements IPathFinder
 				Logger.out.error("CommonPathFinder:", e);
 				//TODO need to see how to handle exception
 			}
+			catch (DAOException e)
+			{
+				// TODO Auto-generated catch block
+				Logger.out.error("CommonPathFinder:", e);
+			}
 			finally
 			{
 				try
 				{
+					if(Variables.isExecutingTestCase)
+					{
+						DB_CONNECTION_PARAMS.commit();
+						DB_CONNECTION_PARAMS.closeSession();
+					}
+					
 					if (connection != null)
 					{
 						connection.close();
 					}
 				}
 				catch (SQLException e)
+				{
+					Logger.out.error("CommonPathFinder:", e);
+				}
+				catch (DAOException e)
 				{
 					Logger.out.error("CommonPathFinder:", e);
 				}
