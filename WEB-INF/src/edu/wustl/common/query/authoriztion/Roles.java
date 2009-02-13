866c62792d11f0ed092cb48e2cda1c8939d6908a
maninder_randhawa <maninder_randhawa>
2009-02-13 14:43:05 +0000

    Removed PMD Errors.
    SVN-Revision: 4991

--- WEB-INF/src/edu/wustl/common/query/authoriztion/Roles.java
+++ WEB-INF/src/edu/wustl/common/query/authoriztion/Roles.java
@@ -2,5 +2,5 @@ package edu.wustl.common.query.authoriztion;
 
 public interface Roles 
 {
-	public static final String EXECUTE_QUERY = "EXECUTE_QUERY";
+	  String EXECUTE_QUERY = "EXECUTE_QUERY";
 }
