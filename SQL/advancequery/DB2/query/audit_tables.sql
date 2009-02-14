922c3f22f00628887068cedff505e92c939c5f92
ravindra_jain <ravindra_jain>
2009-02-14 09:56:07 +0000

    incorporating schema changes
    SVN-Revision: 5049

--- SQL/advancequery/DB2/query/audit_tables.sql
+++ SQL/advancequery/DB2/query/audit_tables.sql
@@ -1,4 +1,22 @@
-create table QUERY_EXECUTION_LOG(CREATIONTIME timestamp, USER_ID integer, STATUS varchar(30), PROJECT_ID integer, QUERY_EXECUTION_ID integer generated always as identity, QUERY_ID integer);
-create table QUERY_ITABLE(PATIENT_DEID integer, UPI varchar(24), QUERY_EXECUTION_ID integer);
-create index indx_execution_log_id on QUERY_ITABLE(QUERY_EXECUTION_ID);
+create table QUERY_EXECUTION_LOG(QUERY_EXECUTION_ID integer, XQUERY_String varchar(32500), QUERY_TYPE char(3), 
+CREATIONTIME timestamp, IP_ADDRESS varchar(50), PROJECT_ID integer);
 
+
+create table COUNT_QUERY_EXECUTION_LOG(COUNT_QUERY_EXECUTION_ID integer generated always as identity, QUERY_ID integer, USER_ID integer, 
+QUERY_COUNT integer, QUERY_STATUS varchar(50));
+
+
+create table DATA_QUERY_EXECUTION_LOG(COUNT_QUERY_EXECUTION_ID integer, DATA_QUERY_EXECUTION_ID integer generated always as identity);
+
+create table QUERY_ITABLE(COUNT_QUERY_EXECUTION_ID integer, PATIENT_DEID integer, UPI char(24), DATE_OF_BIRTH timestamp, VIEW_FLAG char(3));
+
+create table QUERY_SECURITY_LOG(QUERY_EXECUTION_ID integer, SECURITY_CODE varchar(32600));
+
+create index indx_cnt_qry_exec_id on QUERY_ITABLE(COUNT_QUERY_EXECUTION_ID);
+
+CREATE SEQUENCE DEID_SEQUENCE 
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+NO CACHE;
