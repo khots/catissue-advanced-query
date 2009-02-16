81c0bafed5a55a50f48171556c57b988caf75f60
gaurav_sawant <gaurav_sawant>
2009-02-16 14:57:36 +0000

    Added a new method for executing the composite query in a new thread.
    SVN-Revision: 5111

--- WEB-INF/src/edu/wustl/query/querymanager/AbstractQueryManager.java
+++ WEB-INF/src/edu/wustl/query/querymanager/AbstractQueryManager.java
@@ -1,70 +1,94 @@
 /**
- * 
+ *
  */
 package edu.wustl.query.querymanager;
 
 import java.sql.SQLException;
+import java.util.List;
+
 import edu.wustl.common.query.AbstractQuery;
 import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
 import edu.wustl.common.querysuite.exceptions.SqlException;
 import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.query.domain.Workflow;
+import edu.wustl.query.util.querysuite.QueryModuleException;
 
 
 /**
  * @author supriya_dankh
  * The UI layer will use this class for all query backend related operations.
- *  Specific implementations will handle application specific business logic 
+ *  Specific implementations will handle application specific business logic
  *  like for CIDER adding additional predicates (like active UPI flag) into the IQuery object.
- *  
+ *
  *  This class is also responsible for
  *   a.   Maintaining a list of currently running queries
  *   b.   Spawning new threads for new queries
  *   c.   Provide methods to get count, cancel/abort query threads etc.
- *  
+ *
  */
 public abstract class AbstractQueryManager
 {
-	
+
 	/**
-	 * 
+	 *
 	 * @param query
 	 * @return
+	 * @throws QueryModuleException
 	 */
-	public int execute(AbstractQuery query)throws MultipleRootsException,SqlException
+	public int execute(AbstractQuery query)throws MultipleRootsException,SqlException, QueryModuleException
 	{
-		
+
 		return 0;
 	}
-	
+
 	/**
-	 * 
+     * This method is used to execute a composite query.
+     *
+     * @param compositeQuery
+     *            The Composite Query
+     * @param dependendentQueryExecIds
+     *            List containing the <code>query_Execution_ids</code> of the
+     *            dependent queries.
+     * @return The <code>Query_Exec_Id</code> of the composite query.
+     * @throws MultipleRootsException
+     * @throws SqlException
+     * @throws QueryModuleException
+     */
+    public int execute(AbstractQuery compositeQuery,
+            List<Integer> dependendentQueryExecIds)
+            throws MultipleRootsException, SqlException, QueryModuleException
+    {
+        return 0;
+    }
+
+	/**
+	 *
 	 * @param workflow
 	 * @return
 	 */
 	abstract public int execute(Workflow workflow);
-	
+
 	/**
-	 * 
+	 *
 	 * @param query_excecution_id
 	 * @return
-	 * @throws DAOException 
-	 * @throws SQLException 
+	 * @throws DAOException
+	 * @throws SQLException
 	 */
 	abstract public Count getQueryCount(int query_excecution_id) throws DAOException, SQLException;
-	
+
 	/**
-	 * 
+	 *
 	 * @param query_excecution_id
 	 * @return
 	 */
-	abstract public Count getWorkflowCount(int query_excecution_id);	
-	
+	abstract public Count getWorkflowCount(int query_excecution_id);
+
 	/**
-	 * 
+	 *
 	 * @param query_execution_id
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
 	abstract public void cancel(int query_execution_id) throws SQLException;
-	
+
 }
