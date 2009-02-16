45fd994720f7d095fa4fc39296283348d16dfe2c
gaurav_sawant <gaurav_sawant>
2009-02-16 14:52:55 +0000

    Made changes in the Ajax Handler code to manage - a) Execution of PQs and CQs b) Get the current count of the given queries.
    SVN-Revision: 5104

--- WEB-INF/src/edu/wustl/query/action/WorkflowAjaxHandlerAction.java
+++ WEB-INF/src/edu/wustl/query/action/WorkflowAjaxHandlerAction.java
@@ -3,10 +3,10 @@ package edu.wustl.query.action;
 
 import java.io.Writer;
 import java.util.ArrayList;
-
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,15 +18,18 @@ import org.apache.struts.action.ActionMapping;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import edu.wustl.cider.query.CiderWorkFlowDetails;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.dao.AbstractDAO;
+import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.query.factory.AbstractQueryManagerFactory;
-import edu.wustl.common.query.factory.AbstractQueryUIManagerFactory;
 import edu.wustl.common.util.logger.Logger;
 import edu.wustl.query.bizlogic.BizLogicFactory;
 import edu.wustl.query.bizlogic.WorkflowBizLogic;
+import edu.wustl.query.domain.Workflow;
 import edu.wustl.query.querymanager.AbstractQueryManager;
 import edu.wustl.query.querymanager.Count;
 import edu.wustl.query.util.global.Constants;
-import edu.wustl.query.util.querysuite.AbstractQueryUIManager;
 
 /**
  * @author niharika_sharma
@@ -36,59 +39,111 @@ public class WorkflowAjaxHandlerAction extends Action
 {
 
 	/* (non-Javadoc)
-	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, 
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping,
 	 * org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest
 	 * , javax.servlet.http. HttpServletResponse)
 	 */
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
+	@Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
+		WorkflowBizLogic workflowBizLogic=new WorkflowBizLogic();
+		String errormessage=null;
+		//for saving workflow when click execute
+
+
+		// Get the Query information
+        Long queryId=Long.valueOf(request.getParameter("queryId"));
+
+		String queryTitle=request.getParameter("queryTitle");
 		String operation = request.getParameter(Constants.OPERATION);
-
-		String id = request.getParameter("ID");
-		String queryIndex = request.getParameter("queryId");
-
-		if (request.getSession().getAttribute("idList") == null)
-		{
-
-			request.getSession().setAttribute("idList", new HashSet<String>());
-		}
-
 		String state = request.getParameter("state");
-
-
 		Writer writer = response.getWriter();
 		if (operation != null && "execute".equals(operation.trim()))
 		{
+		    // Fetch the current workflow
+	        String workflowId=request.getParameter("workflowId");
 
-			Long queryId = Long.valueOf(id);
+	        Workflow workflow = null;
+            AbstractDAO dao = DAOFactory.getInstance().getDAO(Constants.HIBERNATE_DAO);
+            dao.openSession(null);
+	        try
+	        {
+	            // Get the workflow
+	            workflow = (Workflow) dao.retrieve(Workflow.class.getName(), Long.valueOf(workflowId));
+	        }
+	        finally
+	        {
+	            dao.closeSession();
+	        }
 
-			WorkflowBizLogic workflowBizLogic = (WorkflowBizLogic) BizLogicFactory.getInstance()
+
+
+	        //get the current selected projectId
+	        int project_id = 0;
+	        if (request.getAttribute(Constants.SELECTED_PROJECT) != null
+	                && !(request.getAttribute(Constants.SELECTED_PROJECT))
+	                        .equals(""))
+	        {
+	            project_id = (Integer.valueOf((request
+	                    .getAttribute(Constants.SELECTED_PROJECT).toString())));
+	        }
+
+	        // Get the current User Id
+	        SessionDataBean sessionData = (SessionDataBean) request.getSession().getAttribute(Constants.SESSION_DATA);
+	        Long userId = sessionData.getUserId();
+
+            // Create a workflow details object
+            // FIXME - CiderWorkFlowdetails cannot be accessed from here. Need a
+            // good enough way to create the workflow details object specific to
+            // Cider and AdvancedQuery.
+            CiderWorkFlowDetails workflowdetails = new CiderWorkFlowDetails(
+                    project_id, userId.intValue(), workflow);
+
+
+			 workflowBizLogic = (WorkflowBizLogic) BizLogicFactory.getInstance()
 					.getBizLogic(Constants.WORKFLOW_BIZLOGIC_ID);
-			
+
 			int queryExecId = Integer.valueOf(request.getParameter("executionLogId"));
+			//TO DO
+			// WorkflowBizLogic--->executeGetCountQuery
+			// WorkflowBizLogic--->getCount
+			List<JSONObject> executionQueryResults = new ArrayList<JSONObject>();
+
 			if (queryExecId == 0)
 			{
 
-				queryExecId = workflowBizLogic.executeGetCountQuery(queryId, request);
+
+				//queryExecId = workflowBizLogic.executeGetCountQuery(queryId, request);
+			    Map<Long, Integer> executionIdMap = workflowBizLogic
+                        .executeGetCountQuery(workflowdetails, queryId, request);
+			    Set<Long> titleset=executionIdMap.keySet();
+			    Iterator<Long>  iterator=titleset.iterator();
+				while(iterator.hasNext())
+				{
+				    Long query=iterator.next();
+
+				    Count resultCount=workflowBizLogic.getCount(executionIdMap.get(query));
+                    executionQueryResults.add(createResultJSON(query, resultCount.getCount(),
+                            resultCount.getStatus(), resultCount.getQuery_exection_id()));
+				}
+			}
+			else
+			{
+					Count resultCount=workflowBizLogic.getCount(queryExecId);
+					executionQueryResults.add(createResultJSON(queryId, resultCount.getCount(),
+							resultCount.getStatus(), resultCount.getQuery_exection_id()));
 			}
 
-			Count countObj=	workflowBizLogic.getCount(
-					 queryExecId);
 
-			List<JSONObject> executionQueryResults = new ArrayList<JSONObject>();
+			if (state != null && state.equals("cancel"))
+			{
+				AbstractQueryManager qManager = AbstractQueryManagerFactory.getDefaultAbstractQueryManager();
+				qManager.cancel(queryExecId);
+				//countObj.setStatus("Completed");
 
-				executionQueryResults.add(createResultJSON(queryId,queryIndex, countObj.getCount(),
-						countObj.getStatus(), countObj.getQuery_exection_id()));
 
-				if (state != null && state.equals("cancel"))
-				{
-					AbstractQueryManager qManager = AbstractQueryManagerFactory.getDefaultAbstractQueryManager();
-					qManager.cancel(queryExecId);
-					countObj.setStatus("Completed");
-					
-					
-				}
+			}
 			response.setContentType(Constants.CONTENT_TYPE_TEXT);
 			writer.write(new JSONObject().put("executionQueryResults", executionQueryResults)
 					.toString());
@@ -98,32 +153,33 @@ public class WorkflowAjaxHandlerAction extends Action
 		return null;
 	}
 
-	/**
-	 * @param queryId =Query identifier for which execute request sent   
-	 * @param queryIndex=row number where results to be displayed   
-	 * @param resultCount=value of result count for query
-	 * @returns jsonObject 
-	 * 
-	 * creates the jsonObject for input parameters
-	 */
-	private JSONObject createResultJSON(Long queryId, String queryIndex, int resultCount,
-			String status, int executionLogId)
-	{
-		JSONObject resultObject = null;
-		resultObject = new JSONObject();
-		try
-		{
-			resultObject.append("queryId", queryId);
-			resultObject.append("queryIndex", queryIndex);
-			resultObject.append("queryResult", resultCount);
-			resultObject.append("status", status);
-			resultObject.append("executionLogId", executionLogId);
-		}
-		catch (JSONException e)
-		{
-			Logger.out.info("error in initializing json object " + e);
-		}
+    /**
+     * @param queryId =Query identifier for which execute request sent
+     * @param errormessage
+     * @param workflowId
+     * @param queryIndex=row number where results to be displayed
+     * @param resultCount=value of result count for query
+     * @returns jsonObject
+     *
+     * creates the jsonObject for input parameters
+     */
+    private JSONObject createResultJSON(Long queryId, int resultCount,
+            String status, int executionLogId)
+    {
+        JSONObject resultObject = null;
+        resultObject = new JSONObject();
+        try
+        {
+            resultObject.append("queryId", queryId);
+            resultObject.append("queryResult", resultCount);
+            resultObject.append("status", status);
+            resultObject.append("executionLogId", executionLogId);
+        }
+        catch (JSONException e)
+        {
+            Logger.out.info("error in initializing json object " + e);
+        }
 
-		return resultObject;
-	}
+        return resultObject;
+    }
 }
