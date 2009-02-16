97ed14e3efb26511ea893043cbe4e965000f1e3e
gaurav_sawant <gaurav_sawant>
2009-02-16 14:49:39 +0000

    New Factory class to fetch the specific implementation of the iTable manager class
    SVN-Revision: 5101

---
+++ WEB-INF/src/edu/wustl/common/query/factory/ITableManagerFactory.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu.wustl.common.query.factory;
+
+import edu.wustl.common.query.itablemanager.ITableManager;
+import edu.wustl.common.util.Utility;
+import edu.wustl.query.util.global.Variables;
+
+/**
+ * Factory to return the Query ITableManager instance.
+ * 
+ * @author Gaurav Sawant
+ *
+ */
+public class ITableManagerFactory 
+{
+
+	/**
+	 * Method to create the default instance of class ITableManager. 
+	 * @return The reference of default instance of ITableManager. 
+	 */
+	public static ITableManager getDefaultITableManager()
+	{
+		return (ITableManager) Utility.getObject(Variables.abstractQueryITableManagerClassName);
+	}	
+	
+	/**
+	 * Method to create instance of class ITableManager. 
+	 * @return The reference of ITableManager. 
+	 */
+	public static ITableManager configureDefaultITableManager(String className)
+	{
+		return (ITableManager) Utility.getObject(className);
+	}	
+	
+}
