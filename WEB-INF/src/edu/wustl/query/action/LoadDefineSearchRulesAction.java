ccec956a318d8072de820ec86c2d0d3855f49c85
amit_doshi <amit_doshi>
2009-02-17 10:30:51 +0000

    Changes regarding edit functionality of MED as well as other entities
    SVN-Revision: 5147

--- WEB-INF/src/edu/wustl/query/action/LoadDefineSearchRulesAction.java
+++ WEB-INF/src/edu/wustl/query/action/LoadDefineSearchRulesAction.java
@@ -58,7 +58,7 @@ public class LoadDefineSearchRulesAction extends Action
 		if (entity != null)
 		{
 			request.getSession().removeAttribute(edu.wustl.query.util.global.Constants.SELECTED_CONCEPT_LIST);
-			html = htmlProvider.generateHTML(entity, null,generateHTMLDetails,null);
+			html = htmlProvider.generateHTML(entity, null,generateHTMLDetails);
 		}
 		//	}
 		request.getSession().setAttribute(Constants.ENUMRATED_ATTRIBUTE, generateHTMLDetails.getEnumratedAttributeMap());
