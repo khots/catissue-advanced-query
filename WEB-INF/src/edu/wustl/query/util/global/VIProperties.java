79d9645f2878851d3e1533cc0a581d1db333ddc8
taraben_khoiwala <taraben_khoiwala>
2009-02-17 14:08:08 +0000

    Property class for VI.
    SVN-Revision: 5190

---
+++ WEB-INF/src/edu/wustl/query/util/global/VIProperties.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.wustl.query.util.global;
+
+
+/**
+ * @author taraben_khoiwala
+ *
+ */
+public class VIProperties
+{
+	public static String sourceVocabUrn="";
+	public static String searchAlgorithm="";
+	public static int maxPVsToShow=100;
+	public static int maxToReturnFromSearch=-1;
+	public static String translationAssociation="translation";
+	public static String sourceVocabName="";
+	public static String sourceVocabVersion="";
+	public static String medClassName="MedicalEntityDictionary";
+}
