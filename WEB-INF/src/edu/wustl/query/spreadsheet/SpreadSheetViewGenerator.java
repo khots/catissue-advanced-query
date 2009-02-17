bda596d79e5c9c5f841c91e513ba343fe2c9adaa
vijay_pande <vijay_pande>
2009-02-17 14:06:31 +0000

    Spread sheet view generator class
    SVN-Revision: 5185

---
+++ WEB-INF/src/edu/wustl/query/spreadsheet/SpreadSheetViewGenerator.java
@@ -0,0 +1,137 @@
+
+package edu.wustl.query.spreadsheet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.wustl.cider.querymanager.CiderQueryManager;
+import edu.wustl.cider.util.CiderQueryUIManager;
+import edu.wustl.common.query.factory.AbstractQueryUIManagerFactory;
+import edu.wustl.common.query.queryobject.impl.OutputTreeDataNode;
+import edu.wustl.common.querysuite.queryobject.IOutputAttribute;
+import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
+import edu.wustl.common.querysuite.queryobject.IQuery;
+import edu.wustl.common.querysuite.queryobject.impl.OutputTreeNode;
+import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
+import edu.wustl.common.querysuite.queryobject.impl.metadata.SelectedColumnsMetadata;
+import edu.wustl.common.util.Utility;
+import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.query.queryexecutionmanager.DataQueryResultsBean;
+import edu.wustl.query.util.querysuite.QueryDetails;
+import edu.wustl.query.util.querysuite.QueryModuleError;
+import edu.wustl.query.util.querysuite.QueryModuleException;
+import edu.wustl.query.viewmanager.ViewType;
+
+/**
+ * @author vijay_pande
+ *
+ */
+public class SpreadSheetViewGenerator
+{
+
+	private org.apache.log4j.Logger logger = Logger.getLogger(SpreadSheetViewGenerator.class);
+
+	protected String dataTypeString;
+	protected String idOfClickedNode;
+	protected QueryDetails queryDetails;
+	protected SelectedColumnsMetadata selectedColumnsMetadata;
+
+	public SpreadSheetData createSpreadSheet()
+	{
+		return null;
+	}
+
+	public OutputTreeNode getClickedNode()
+	{
+		return null;
+	}
+
+	public void updateSpreadSheetForDataNode(SpreadSheetData spreadSheetData)
+	{
+
+	}
+
+	public void updateViewOfQuery(ViewType viewType)
+	{
+
+	}
+
+	/**
+	 * @param queryDetailsObj
+	 * @param idOfClickedNode2
+	 * @param spreadsheetData
+	 * @param request 
+	 * @throws QueryModuleException 
+	 */
+	public void createSpreadsheet(QueryDetails queryDetailsObj, String idOfClickedNode2,
+			SpreadSheetData spreadsheetData, HttpServletRequest request)
+			throws QueryModuleException
+	{
+		Node node = new Node(idOfClickedNode2);
+		OutputTreeDataNode currentTreeNode = null; //queryDetailsObj.getUniqueIdNodesMap().get(uniqueCurrentNodeId);
+
+		List<IOutputAttribute> selectedColumns = ((IParameterizedQuery) queryDetailsObj.getQuery())
+				.getOutputAttributeList(); //getSelectedOutputAttributeList(currentTreeNode,constraints);
+		SpreadsheetIQueryGenerator queryGenerator = new SpreadsheetIQueryGenerator();
+		List<Object> upis = queryGenerator.createIQuery(node, queryDetailsObj, selectedColumns);
+
+		executeXQuery(queryDetailsObj.getQuery(), spreadsheetData, upis, request, queryDetailsObj
+				.getQueryExecutionId());
+
+		List<String> columnsList = getColumnList(queryDetailsObj);
+
+		spreadsheetData.setColumnsList(columnsList);
+
+	}
+
+	/**
+	 * @param queryDetailsObj
+	 * @return
+	 */
+	private List<String> getColumnList(QueryDetails queryDetailsObj)
+	{
+		List<IOutputAttribute> outputAttributeList = ((ParameterizedQuery) queryDetailsObj
+				.getQuery()).getOutputAttributeList();
+		List<String> columnsList = new ArrayList<String>();
+		for (IOutputAttribute outputAttribute : outputAttributeList)
+		{
+			String className = outputAttribute.getAttribute().getEntity().getName();
+			className = Utility.parseClassName(className);
+			String attrLabel = Utility.getDisplayLabel(outputAttribute.getAttribute().getName());
+			columnsList.add(attrLabel + " : " + className);
+		}
+		return columnsList;
+	}
+
+	private void executeXQuery(IQuery query, SpreadSheetData spreadsheetData, List<Object> upis,
+			HttpServletRequest request, int queryExecutionId) throws QueryModuleException
+	{
+		//getData
+		CiderQueryUIManager ciderQueryUIManager = (CiderQueryUIManager) AbstractQueryUIManagerFactory
+				.configureDefaultAbstractUIQueryManager(this.getClass(), request, query);
+		CiderQueryManager ciderQueryManager = new CiderQueryManager();
+
+		DataQueryResultsBean dataQueryResultsBean;
+		try
+		{
+			dataQueryResultsBean = ciderQueryManager.execute(ciderQueryUIManager.getCiderQuery(), upis,
+					queryExecutionId);
+			spreadsheetData.setDataList(dataQueryResultsBean.getAttributeList());
+			spreadsheetData.setDataTypeList(dataQueryResultsBean.getDataTypesList());
+		}
+		catch (DAOException e)
+		{
+			throw new QueryModuleException(e.getMessage(), QueryModuleError.DAO_EXCEPTION);
+		}
+		catch (SQLException e)
+		{
+			throw new QueryModuleException(e.getMessage(), QueryModuleError.SQL_EXCEPTION);
+		}
+			
+		
+	}
+}
