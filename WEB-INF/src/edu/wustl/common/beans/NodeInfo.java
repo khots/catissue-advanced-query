7d4d712eefbcebe93c81f39a4f8a245f03be4988
baljeet_dhindhwal <baljeet_dhindhwal>
2009-02-17 17:31:29 +0000

    Added For Storing results
    SVN-Revision: 5228

---
+++ WEB-INF/src/edu/wustl/common/beans/NodeInfo.java
@@ -0,0 +1,45 @@
+package edu.wustl.common.beans;
+
+import java.sql.Date;
+
+
+public class NodeInfo
+{
+	Object obj;
+	Date dob;
+	
+	/**
+	 * @return the obj
+	 */
+	public Object getObj()
+	{
+		return obj;
+	}
+	
+	/**
+	 * @param obj the obj to set
+	 */
+	public void setObj(Object obj)
+	{
+		this.obj = obj;
+	}
+	
+	/**
+	 * @return the dob
+	 */
+	public Date getDob()
+	{
+		return dob;
+	}
+	
+	/**
+	 * @param dob the dob to set
+	 */
+	public void setDob(Date dob)
+	{
+		this.dob = dob;
+	}
+	
+	
+
+}
