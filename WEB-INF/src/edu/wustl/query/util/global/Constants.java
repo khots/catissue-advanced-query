e02b8079ef0a732c2242f236a2394d0ea4781059
amit_doshi <amit_doshi>
2009-02-20 06:59:46 +0000

    changes regarding edit functionality of VI
    SVN-Revision: 5378

--- WEB-INF/src/edu/wustl/query/util/global/Constants.java
+++ WEB-INF/src/edu/wustl/query/util/global/Constants.java
@@ -618,7 +618,7 @@ public class Constants extends edu.wustl.common.util.global.Constants
 	/**  Constant for  */
 	public static final String NO_RESULT = "No results found";
 	/**  Constant for  */
-	public static final String MED_PV_HTML = "MEDPVHTML";
+	public static final String PV_HTML = "PVHTML";
 
 	/**  Constant for  */
 	public static final String DEFINE_VIEW_MSG = "DefineView";
@@ -762,7 +762,7 @@ public class Constants extends edu.wustl.common.util.global.Constants
 	public static final String MED_MAPPED_N_NOT_VALIED_PVCONCEPT = "Bold_Italic_Disabled";
 	public static final String NOT_MED_MAPPED_PVCONCEPT = "Normal_Italic_Disabled";
 	public static final String NOT_MED_VALED_PVCONCEPT = "Normal_Disabled";
-	public static final String ID_DEL = "~ID_DEL~"; 
+	public static final String ID_DEL = "~ID_DEL~"; // if you change its value,kindly change in queryModule.js its hard coded there 
 	
 	/**
 	 * Query ITABLE
