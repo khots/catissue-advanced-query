a44777f443ca35da69a1a916eee28926b3f9ef6f
chitra_garg <chitra_garg>
2009-02-20 08:12:02 +0000

    Table changes for count queries
    SVN-Revision: 5383

--- WEB-INF/src/edu/wustl/query/action/RetrieveRecentQueriesAction.java
+++ WEB-INF/src/edu/wustl/query/action/RetrieveRecentQueriesAction.java
@@ -17,6 +17,7 @@ import edu.wustl.common.dao.AbstractDAO;
 import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.JDBCDAO;
 import edu.wustl.common.dao.queryExecutor.PagenatedResultData;
+import edu.wustl.common.querysuite.queryobject.impl.AbstractQuery;
 import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
 import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.query.actionForm.ShowRetrieveRecentForm;
@@ -52,7 +53,7 @@ public class RetrieveRecentQueriesAction extends Action
 	{
 		SessionDataBean sessionDataBean = (SessionDataBean) request.getSession().getAttribute(
 				Constants.SESSION_DATA);
-		String sql = "select count(*) from  QUERY_EXECUTION_LOG where USER_ID= "
+		String sql = "select count(*) from  COUNT_QUERY_EXECUTION_LOG where USER_ID= "
 				+ sessionDataBean.getUserId();
 		int numberOfQueries = 0;
 		List<List<String>> resultCount = executeQuery(sql, sessionDataBean, false, false, null, -1,
@@ -165,8 +166,12 @@ public class RetrieveRecentQueriesAction extends Action
 	private List<List<String>> retrieveQueries(SessionDataBean sessionDataBean, int pageNum,
 			int recordsPerPage, int lastIndex) throws QueryModuleException
 	{
-		String sql = "select * from  QUERY_EXECUTION_LOG where USER_ID="
-				+ sessionDataBean.getUserId() + " order by CREATIONTIME desc ";
+//		String sql = "select * from  COUNT_QUERY_EXECUTION_LOG where USER_ID="
+//				+ sessionDataBean.getUserId() + " order by CREATIONTIME desc ";
+//		
+		String sql ="SELECT * "+
+		   "FROM QUERY_EXECUTION_LOG qel, COUNT_QUERY_EXECUTION_LOG cqel "+
+		   "WHERE qel.QUERY_EXECUTION_ID = cqel.COUNT_QUERY_EXECUTION_ID and cqel.USER_ID="+sessionDataBean.getUserId() + "  order by qel.CREATIONTIME desc ";
 		List<List<String>> queryResultList = executeQuery(sql, sessionDataBean, false, false, null,
 				(pageNum - 1) * recordsPerPage, lastIndex);
 		return queryResultList;
@@ -187,14 +192,14 @@ public class RetrieveRecentQueriesAction extends Action
 		for (List<String> parameterizedQuery : queryResultList)
 		{
 			RecentQueriesBean recentQueriesBean = new RecentQueriesBean();
-			recentQueriesBean.setQueryCreationDate(parameterizedQuery.get(0));//new Timestamp((new Date()).getTime()));
-			recentQueriesBean.setResultCount(100L);
-			recentQueriesBean.setStatus(parameterizedQuery.get(2));
+			recentQueriesBean.setQueryCreationDate(parameterizedQuery.get(3));//(0));
+			recentQueriesBean.setResultCount(Long.valueOf(parameterizedQuery.get(9)));//(100L);
+			recentQueriesBean.setStatus(parameterizedQuery.get(10));//2));
 
-			String title = retrieveQueryName(Long.valueOf(parameterizedQuery.get(5)),
-					sessionDataBean);//query id from listparameterizedQuery.get(5)
+			String title = retrieveQueryName(Long.valueOf(parameterizedQuery.get(7)),
+					sessionDataBean);
 			recentQueriesBean.setQueryTitle(title);
-			recentQueriesBean.setQueyExecutionId(Long.valueOf(parameterizedQuery.get(4)));
+			recentQueriesBean.setQueyExecutionId(Long.valueOf(parameterizedQuery.get(0)));//Long.valueOf(parameterizedQuery.get(4)));
 			recentQueriesBeanList.add(recentQueriesBean);
 		}
 		return recentQueriesBeanList;
@@ -274,11 +279,11 @@ public class RetrieveRecentQueriesAction extends Action
 	private String retrieveQueryName(Long id, SessionDataBean sessionDataBean) throws DAOException
 	{
 
-		String sourceObjectName = ParameterizedQuery.class.getName();
+		String sourceObjectName = AbstractQuery.class.getName();
 		AbstractDAO dao = DAOFactory.getInstance().getDAO(Constants.HIBERNATE_DAO);
 		dao.openSession(sessionDataBean);
 
-		String name = ((ParameterizedQuery) dao.retrieve(sourceObjectName, id)).getName();
+		String name = ((AbstractQuery) dao.retrieve(sourceObjectName, id)).getName();
 		dao.closeSession();
 
 		return name;
