663f6ef9e2319febede14a068d112026a8c7a2b9
taraben_khoiwala <taraben_khoiwala>
2009-02-20 13:42:56 +0000

    Changed the "~ID_DEL~" to "ID_DEL"
    SVN-Revision: 5425

--- WEB-INF/src/edu/wustl/query/util/global/Constants.java
+++ WEB-INF/src/edu/wustl/query/util/global/Constants.java
@@ -766,9 +766,8 @@ public class Constants extends edu.wustl.common.util.global.Constants
 	public static final String MED_MAPPED_N_NOT_VALIED_PVCONCEPT = "Bold_Italic_Disabled";
 	public static final String NOT_MED_MAPPED_PVCONCEPT = "Normal_Italic_Disabled";
 	public static final String NOT_MED_VALED_PVCONCEPT = "Normal_Disabled";
-	public static final String ID_DEL = "~ID_DEL~"; // if you change its value,kindly change in queryModule.js its hard coded there
+	public static final String ID_DEL = "ID_DEL"; 
 	public static final String MSG_DEL = "@MSG@"; // if you change its value,kindly change in queryModule.js its hard coded there
-	
 	
 	/**
 	 * Query ITABLE
@@ -808,11 +807,6 @@ public class Constants extends edu.wustl.common.util.global.Constants
     public static final String QRY_COUNT = "QUERY_COUNT";
     /**  Constant for DEID_SEQUENCE **/
     public static final String DEID_SEQUENCE = "DEID_SEQUENCE";
-    /**  Constant for  ITABLE_ATTRIBUTES**/
-    public static final String TAGGED_VALUE_ITABLE_ATTRIBUTES = "ITABLE_ATTRIBUTES";
-    /**  Constant for SECURE CONDITION **/
-    public static final String TAGGED_VALUE_SECURE_CONDITION = "SECURE_CONDITION";
     
-    public static final int AGE = 89;
-    public static final int MINOR = 18;
+    public static final String TAGGED_VALUE_ITABLE_ATTRIBUTES = "ITABLE_ATTRIBUTES";
 }
