1003fda061f310d86599fc1ddef1c2e254a67270
vijay_pande <vijay_pande>
2009-02-24 08:29:23 +0000

    getSelectedColumnList(IQuery query)  method added
    SVN-Revision: 5457

--- WEB-INF/src/edu/wustl/query/viewmanager/UserDefinedViewManager.java
+++ WEB-INF/src/edu/wustl/query/viewmanager/UserDefinedViewManager.java
@@ -3,6 +3,12 @@
  */
 package edu.wustl.query.viewmanager;
 
+import java.util.List;
+
+import edu.wustl.common.querysuite.queryobject.IOutputAttribute;
+import edu.wustl.common.querysuite.queryobject.IQuery;
+import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
+import edu.wustl.query.util.querysuite.QueryModuleError;
 import edu.wustl.query.util.querysuite.QueryModuleException;
 
 
@@ -15,5 +21,16 @@ public class UserDefinedViewManager extends ViewManager
 	public void createSpreadSheetView() throws QueryModuleException
 	{
 		
+	}
+	
+	public List<IOutputAttribute> getSelectedColumnList(IQuery query) throws QueryModuleException
+	{
+		List<IOutputAttribute> outputAttributes =((ParameterizedQuery)query).getOutputAttributeList();
+		if(outputAttributes==null || outputAttributes.size()==0)
+		{
+			throw new QueryModuleException("No output attribute defined for query using Define View!", QueryModuleError.GENERIC_EXCEPTION);
+		}
+		
+		return ((ParameterizedQuery)query).getOutputAttributeList();
 	}
 }
