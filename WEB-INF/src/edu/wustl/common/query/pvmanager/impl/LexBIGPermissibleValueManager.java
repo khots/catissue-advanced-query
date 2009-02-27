c330e5de180b5b9cc0abf2fe5cc57aeaa945294e
taraben_khoiwala <taraben_khoiwala>
2009-02-27 12:55:07 +0000

    Removed showListBoxForPV method.Updated the showIcon,getTaggedValueForAttribute and getPermissibleValue method as the chages had been made in MedLookUpManager.
    SVN-Revision: 5656

--- WEB-INF/src/edu/wustl/common/query/pvmanager/impl/LexBIGPermissibleValueManager.java
+++ WEB-INF/src/edu/wustl/common/query/pvmanager/impl/LexBIGPermissibleValueManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Properties;
 
 import edu.common.dynamicextensions.domain.StringValue;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
@@ -13,121 +13,52 @@ import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.common.dynamicextensions.domaininterface.PermissibleValueInterface;
 import edu.common.dynamicextensions.domaininterface.TaggedValueInterface;
 import edu.wustl.common.query.pvmanager.IPermissibleValueManager;
-import edu.wustl.common.util.logger.Logger;
 import edu.wustl.common.vocab.IVocabulary;
 import edu.wustl.common.vocab.IVocabularyManager;
 import edu.wustl.common.vocab.VocabularyException;
 import edu.wustl.common.vocab.impl.VocabularyManager;
+import edu.wustl.common.vocab.utility.VocabUtil;
 import edu.wustl.query.util.global.Constants;
 
 public class LexBIGPermissibleValueManager implements IPermissibleValueManager
 {
-	public List<PermissibleValueInterface> getPermissibleValueList(final AttributeInterface attribute,
-			final EntityInterface entity) throws PVManagerException 
+
+	public List<PermissibleValueInterface> getPermissibleValueList(
+			final AttributeInterface attribute, final EntityInterface entity)
+			throws PVManagerException
 	{
 
 		List<PermissibleValueInterface> permissibleValueList = new ArrayList<PermissibleValueInterface>();
 		if (isEnumerated(attribute, entity))
 		{
-			List<String> filter = getTaggedValueForAttribute(attribute, entity);
+			String filter = getTaggedValueForAttribute(attribute, entity,Constants.PERMISSIBLEVALUEFILTER);
 			//fetch pv_view from here 
-			String view =	getTaggedValueForView(attribute, entity);
+			String view = getTaggedValueForAttribute(attribute, entity,Constants.PERMISSIBLEVALUEVIEW);
 			MedLookUpManager medManager = MedLookUpManager.instance();
-			List<String> medPermissibleValueList = medManager.getPermissibleValues(filter,view);//(filter,view)
+			List<String> medPermissibleValueList = medManager.getPermissibleValues(filter, view);//(filter,view)
 			permissibleValueList = new ArrayList<PermissibleValueInterface>();
-			if(medPermissibleValueList!=null)
+			if (medPermissibleValueList != null)
 			{
-				for(String conceptCode:medPermissibleValueList)
+				for (String conceptCode : medPermissibleValueList)
 				{
-					StringValue pv= new StringValue();
-					pv.setValue(conceptCode);				
+					StringValue pv = new StringValue();
+					pv.setValue(conceptCode);
 					permissibleValueList.add(pv);
 				}
 			}
-			
+
 		}
 		return permissibleValueList;
 
 	}
+
 	
-	/*public List<PermissibleValueInterface> getPermissibleValueList(final AttributeInterface attribute, final EntityInterface entity) 
-	{
-
-		List<String> medPermissibleValueList = new ArrayList<String>();
-		List<IConcept> permissiblevalueList = null;
-		
-		if (isEnumerated(attribute, entity))
-		{
-			String filter = getPVFilterValueForAttribute(attribute, entity);
-			//call taras method to get concept codes from MED lookup table
-			MedLookUpManager medManager = MedLookUpManager.instance();
-			medPermissibleValueList = medManager.getPermissibleValues(filter);
-			if (medPermissibleValueList != null)
-			{
-
-				IVocabularyManager vocabMngr = VocabularyManager.getInstance();
-
-				Properties vocabProps = VocabUtil.getVocabProperties();
-				//list of coded entries !!!
-				try
-				{
-					permissiblevalueList = vocabMngr.getConceptDetails(medPermissibleValueList, new Vocabulary(vocabProps
-							.getProperty("source.vocab.name"), vocabProps.getProperty("source.vocab.version")));
-				}
-				catch (VocabularyException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			}
-		}
-		return convertToPermissiblevalue(permissiblevalueList);
-
-	}*/
-
-	/**
-	 * method to decide whether to show a listbox for the permissible values
-	 * @throws PVManagerException 
-	 * @throws VocabularyException 
-	 */
-	public boolean showListBoxForPV(AttributeInterface attribute, EntityInterface entity) throws PVManagerException
-	{
-
-		boolean showListBox = false;
-		IVocabularyManager vocabMngr= VocabularyManager.getInstance();
-		
-		if (isEnumerated(attribute, entity))
-		{
-			List<IVocabulary> vocabularies;
-			try
-			{
-				vocabularies = vocabMngr.getConfiguredVocabularies();
-			}
-			catch (VocabularyException e)
-			{
-				throw new PVManagerException("Error occured while checking the available vocabularies.",e);
-			}
-			int noOfCodingSchemes = vocabularies.size();
-			List<String> pvFilter = getTaggedValueForAttribute(attribute, entity);
-			String view =	getTaggedValueForView(attribute, entity);
-			MedLookUpManager medManager = MedLookUpManager.instance();
-			List<String> toReturn = medManager.getPermissibleValues(pvFilter,view);
-			if (toReturn != null && toReturn.size() < 10 && noOfCodingSchemes == 1)
-			{
-				showListBox = true;
-			}
-		}
-		
-		return showListBox;
-
-	}
-
+	
 	/**
 	 * check whether attribute is enumerated.
 	 * 
 	 */
-	public boolean isEnumerated(AttributeInterface attribute,EntityInterface entity)
+	public boolean isEnumerated(AttributeInterface attribute, EntityInterface entity)
 	{
 		Collection<TaggedValueInterface> tagList = entity.getTaggedValueCollection();
 		Iterator<TaggedValueInterface> tagIterator = tagList.iterator();
@@ -140,8 +71,8 @@ public class LexBIGPermissibleValueManager implements IPermissibleValueManager
 				//entity.getParentEntity().getName().eq
 				TaggedValueInterface temp = (TaggedValueInterface) tagIterator.next();
 
-				if (temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEFILTER) || 
-						temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEVIEW))
+				if (temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEFILTER)
+						|| temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEVIEW))
 				{
 
 					isEnumerated = true;
@@ -155,85 +86,62 @@ public class LexBIGPermissibleValueManager implements IPermissibleValueManager
 	}
 
 	/**
-	 * utility list to convert a list of codedEntries to permissiblevalues 
-	 * @param inputList
-	 * @return
-	 *//*
-	private List<PermissibleValueInterface> convertToPermissiblevalue(List<IConcept> inputList)
-	{
-		ListIterator<IConcept> iterator = inputList.listIterator();
-		List<PermissibleValueInterface> pvList = new ArrayList<PermissibleValueInterface>();
-		while (iterator.hasNext())
-		{
-			IConcept concept = iterator.next();
-			ConceptValue conceptV = new ConceptValue();
-			//assign the values from coded entry to concept values
-			conceptV.setConceptCode(concept.getCode());
-			conceptV.setConceptDescription(concept.getDescription());
-
-			pvList.add((PermissibleValueInterface) conceptV);
-		}
-		return pvList;
-	}
-*/
-	/**
 	 * Get the permissible value filter for an attribute
 	 * @param attribute
 	 * @return
 	 */
 
-	public List<String> getTaggedValueForAttribute(final AttributeInterface attribute, final EntityInterface entity)//,final String tagName)
+	public String getTaggedValueForAttribute(final AttributeInterface attribute,
+			final EntityInterface entity,final String tagName)
 	{
-		List<String> taggedValuesList = null;
-		//String taggedValue =""; 
+		String taggedValue = null; 
 		Collection<TaggedValueInterface> tagList = entity.getTaggedValueCollection();
 		Iterator<TaggedValueInterface> tagIterator = tagList.iterator();
 		while (tagIterator.hasNext())
 		{
 			TaggedValueInterface temp = (TaggedValueInterface) tagIterator.next();
-			if (temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEFILTER))
+			if (temp.getKey().toString().equals(tagName))
 			{
-				taggedValuesList = new ArrayList<String>();
-				String pvFilterValue = temp.getValue();
-				StringTokenizer tokenizer = new StringTokenizer(pvFilterValue,",",false);
-				while(tokenizer.hasMoreTokens())
-				{
-					taggedValuesList.add(tokenizer.nextToken());
-				}
+				taggedValue = temp.getValue();
 				break;
-				//taggedValue = temp.getValue().toString();
-			}
-		}
-		return taggedValuesList;
-	}
-	
-	/**
-	 * to fetch the view associated with the entity
-	 */
-	
-	public String getTaggedValueForView(final AttributeInterface attribute, final EntityInterface entity)
-	{
-		
-		String taggedValue =""; 
-		Collection<TaggedValueInterface> tagList = entity.getTaggedValueCollection();
-		Iterator<TaggedValueInterface> tagIterator = tagList.iterator();
-		while (tagIterator.hasNext())
-		{
-			TaggedValueInterface temp = (TaggedValueInterface) tagIterator.next();
-			if (temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEVIEW))
-			{
-				
-				taggedValue = temp.getValue().toString();
 			}
 		}
 		return taggedValue;
 	}
+
+	//Commented By: Tara Khoiwal
+	//Reason:As the filtering condition SQL is moved to metadata so no need of tokenizing the pv_filter tag.
+	//	public List<String> getTaggedValueForAttribute(final AttributeInterface attribute, final EntityInterface entity)//,final String tagName)
+	//	{
+	//		List<String> taggedValuesList = null;
+	//		//String taggedValue =""; 
+	//		Collection<TaggedValueInterface> tagList = entity.getTaggedValueCollection();
+	//		Iterator<TaggedValueInterface> tagIterator = tagList.iterator();
+	//		while (tagIterator.hasNext())
+	//		{
+	//			TaggedValueInterface temp = (TaggedValueInterface) tagIterator.next();
+	//			if (temp.getKey().toString().equals(Constants.PERMISSIBLEVALUEFILTER))
+	//			{
+	//				taggedValuesList = new ArrayList<String>();
+	//				String pvFilterValue = temp.getValue();
+	//				StringTokenizer tokenizer = new StringTokenizer(pvFilterValue,",",false);
+	//				while(tokenizer.hasMoreTokens())
+	//				{
+	//					taggedValuesList.add(tokenizer.nextToken());
+	//				}
+	//				break;
+	//				//taggedValue = temp.getValue().toString();
+	//			}
+	//		}
+	//		return taggedValuesList;
+	//	}
+	//	
 	
 	/**
 	 * 
 	 */
-	public boolean showIcon(AttributeInterface attribute, EntityInterface entity,
-			List<PermissibleValueInterface> permissibleValues) throws PVManagerException
+	public boolean showIcon(AttributeInterface attribute, EntityInterface entity)
+			throws PVManagerException
 	{
 		boolean showIcon = true;
 		try
@@ -241,14 +149,15 @@ public class LexBIGPermissibleValueManager implements IPermissibleValueManager
 			IVocabularyManager vocabMngr = VocabularyManager.getInstance();
 			if (isEnumerated(attribute, entity))
 			{
-				List<IVocabulary> vocabularies = vocabMngr.getConfiguredVocabularies();
-				int noOfCodingSchemes = vocabularies.size();
-				/*List<String> pvFilter = getTaggedValueForAttribute(attribute, entity);
-				String view =	getTaggedValueForView(attribute, entity);
+				Properties properties = VocabUtil.getConfigVocabProps();
+				
+				//List<IVocabulary> vocabularies = vocabMngr.getConfiguredVocabularies();
+				int noOfCodingSchemes = properties.size();
+				String pvFilter = getTaggedValueForAttribute(attribute, entity,Constants.PERMISSIBLEVALUEFILTER);
+				String view = getTaggedValueForAttribute(attribute, entity,Constants.PERMISSIBLEVALUEVIEW);
 				MedLookUpManager medManager = MedLookUpManager.instance();
-				List<String> toReturn = medManager.getPermissibleValues(pvFilter,view);*/
-				List<PermissibleValueInterface> toReturn = permissibleValues;
-				if (toReturn != null && toReturn.size() < 5 && noOfCodingSchemes == 1)
+				int pvCount = medManager.getPermissibleValuesCount(pvFilter, view);
+				if (pvCount < 10 && noOfCodingSchemes == 1)
 				{
 					showIcon = false;
 				}
@@ -256,7 +165,7 @@ public class LexBIGPermissibleValueManager implements IPermissibleValueManager
 		}
 		catch (VocabularyException e)
 		{
-			Logger.out.error(e.getMessage(),e);
+			throw new PVManagerException(e.getError().getErrorMessage(), e);
 		}
 		return showIcon;
 
