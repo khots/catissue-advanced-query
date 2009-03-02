1d845f51c179eed62566c7ee785ae89f4f9801ba
pavan_kalantri <pavan_kalantri>
2009-03-02 09:11:37 +0000

    removed PMD
    SVN-Revision: 5729

--- WEB-INF/src/edu/wustl/query/action/LoadQueryPageAction.java
+++ WEB-INF/src/edu/wustl/query/action/LoadQueryPageAction.java
@@ -2,14 +2,17 @@ package edu.wustl.query.action;
 
 import java.io.IOException;
 import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+
 import edu.wustl.common.util.Utility;
 import edu.wustl.query.util.global.Constants;
 
