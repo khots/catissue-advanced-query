d203eb0c97aa198cea43d5357b26ef4e238fe33d
pavan_kalantri <pavan_kalantri>
2009-03-02 08:01:09 +0000

    removed PMD
    SVN-Revision: 5717

--- WEB-INF/src/edu/wustl/query/bizlogic/QueryBizLogic.java
+++ WEB-INF/src/edu/wustl/query/bizlogic/QueryBizLogic.java
@@ -4,20 +4,13 @@
 
 package edu.wustl.query.bizlogic;
 
-import java.util.HashSet;
-
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.bizlogic.DefaultBizLogic;
 import edu.wustl.common.dao.DAO;
 import edu.wustl.common.exception.BizLogicException;
-import edu.wustl.common.query.authoriztion.SavedQueryAuthorization;
 import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
-import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
-import edu.wustl.common.security.PrivilegeUtility;
 import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
 import edu.wustl.common.util.dbManager.DAOException;
-import gov.nih.nci.security.authorization.domainobjects.User;
-import gov.nih.nci.security.exceptions.CSException;
 
 /**
  * @author chetan_patil
