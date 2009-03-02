b3ea5a848eceed815c05d32a8cebdaae2f6343be
pavan_kalantri <pavan_kalantri>
2009-03-02 09:11:18 +0000

    removed PMD
    SVN-Revision: 5728

--- WEB-INF/src/edu/wustl/query/action/CategorySearchAction.java
+++ WEB-INF/src/edu/wustl/query/action/CategorySearchAction.java
@@ -11,7 +11,6 @@ import java.util.StringTokenizer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
