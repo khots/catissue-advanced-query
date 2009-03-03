f77bc45b01f6606ad2d1bbdc6e435da5a97db4c4
chitra_garg <chitra_garg>
2009-03-03 10:27:41 +0000

    csm related changes
    SVN-Revision: 5788

---
+++ WEB-INF/src/edu/wustl/query/util/querysuite/CsmUtility.java
@@ -0,0 +1,97 @@
+package edu.wustl.query.util.querysuite;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
+import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
+import edu.wustl.common.security.PrivilegeCache;
+import edu.wustl.common.security.PrivilegeManager;
+import edu.wustl.common.security.PrivilegeUtility;
+import edu.wustl.common.util.Permissions;
+import edu.wustl.query.util.global.Constants;
+import gov.nih.nci.security.authorization.domainobjects.ProtectionElement;
+import gov.nih.nci.security.authorization.domainobjects.ProtectionGroup;
+import gov.nih.nci.security.authorization.domainobjects.User;
+import gov.nih.nci.security.dao.ProtectionElementSearchCriteria;
+import gov.nih.nci.security.exceptions.CSException;
+import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
+
+public class CsmUtility {
+	
+	private PrivilegeCache setPrivilegeCache(SessionDataBean sessionDataBean ) throws CSException
+	{
+		User user = new PrivilegeUtility().getUserProvisioningManager().getUser(sessionDataBean.getUserName());
+		sessionDataBean.setCsmUserId(user.getUserId().toString());
+		PrivilegeCache privilegeCache = PrivilegeManager.getInstance().getPrivilegeCache(sessionDataBean.getUserName());
+		return privilegeCache;
+
+	}
+	public void  checkExecuteQueryPrivilege(
+			Collection<IParameterizedQuery> parameterizedQueryCollection,			
+			Collection<IParameterizedQuery> authorizedQueryCollection,
+			Collection<IParameterizedQuery> sharedQueryCollection,
+			SessionDataBean sessionDataBean ) throws CSException,
+			CSObjectNotFoundException
+	{
+		PrivilegeCache privilegeCache =setPrivilegeCache(sessionDataBean);
+		for(IParameterizedQuery parameterizedQuery : parameterizedQueryCollection)
+		{
+			String objectId = ((ParameterizedQuery)parameterizedQuery).getObjectId();
+			if(privilegeCache.hasPrivilege(objectId, Permissions.EXECUTE_QUERY))
+			{
+				boolean sharedQuery = setSharedQueriesCollection(sharedQueryCollection,
+						parameterizedQuery, objectId);
+				if(!sharedQuery)
+				{
+					authorizedQueryCollection.add(parameterizedQuery);
+				}
+			}
+		}
+	}
+	
+	private boolean setSharedQueriesCollection(
+			Collection<IParameterizedQuery> sharedQueryCollection,
+			IParameterizedQuery parameterizedQuery, String objectId) throws CSException,
+			CSObjectNotFoundException
+	{
+		ProtectionElement pe = new ProtectionElement();
+
+		List<ProtectionElement> peList = new ArrayList<ProtectionElement>();
+		PrivilegeUtility privilegeUtility = new PrivilegeUtility();
+		pe.setObjectId(objectId);
+		ProtectionElementSearchCriteria searchCriteria = new ProtectionElementSearchCriteria(pe);
+		peList = privilegeUtility.getUserProvisioningManager().getObjects(searchCriteria);
+		boolean sharedQuery = false;
+		if (peList != null && !peList.isEmpty())
+		{
+			pe = peList.get(0);
+		}
+		
+		Set<ProtectionGroup> pgSet = privilegeUtility.getUserProvisioningManager().getProtectionGroups(pe.getProtectionElementId().toString());
+		
+		sharedQuery = populateSharedQueryCollection(sharedQueryCollection, parameterizedQuery,
+				pgSet);
+		return sharedQuery;
+	}
+	
+	private boolean populateSharedQueryCollection(
+			Collection<IParameterizedQuery> sharedQueryCollection,
+			IParameterizedQuery parameterizedQuery, Set<ProtectionGroup> pgSet)
+	{
+		boolean sharedQuery=false;
+		for(ProtectionGroup pg : pgSet)
+		{
+			if(pg.getProtectionGroupName().equals(Constants.PUBLIC_QUERY_PROTECTION_GROUP))
+			{
+				sharedQueryCollection.add(parameterizedQuery);
+				sharedQuery = true;
+			}
+		}
+		return sharedQuery;
+	}
+	
+
+}
