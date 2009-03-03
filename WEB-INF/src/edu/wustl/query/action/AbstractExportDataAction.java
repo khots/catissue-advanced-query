3b28d150539517ea1735579ec5daeecbe51b9537
ravindra_jain <ravindra_jain>
2009-03-03 12:20:52 +0000

    export data functionality changes
    SVN-Revision: 5806

--- WEB-INF/src/edu/wustl/query/action/AbstractExportDataAction.java
+++ WEB-INF/src/edu/wustl/query/action/AbstractExportDataAction.java
@@ -8,8 +8,10 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-
 import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.query.AbstractQuery;
+import edu.wustl.common.util.global.Constants;
+import edu.wustl.query.exportmanager.ExportDataManager;
 import edu.wustl.query.exportmanager.ExportDataObject;
 
 /**
@@ -21,18 +23,46 @@ import edu.wustl.query.exportmanager.ExportDataObject;
  */
 public abstract class AbstractExportDataAction extends Action
 {
+
 	/**
 	 * 
 	 * @param exportDataObject
 	 * @param sessionDataBean
 	 */
-	protected abstract void preProcess(ExportDataObject exportDataObject, SessionDataBean sessionDataBean);
-	
+	protected abstract void preProcess(ExportDataObject exportDataObject,
+			SessionDataBean sessionDataBean);
+
 	@Override
-	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm,
+	public final ActionForward execute(ActionMapping actionMapping, ActionForm actionForm,
 			HttpServletRequest request, HttpServletResponse response)
 	{
+		ExportDataObject exportDataObject = new ExportDataObject();
+		SessionDataBean sessionDataBean = getSessionData(request);
+			
+		AbstractQuery abstractQuery = (AbstractQuery) request.getSession().getAttribute("");
+		exportDataObject.getExportObjectDetails().put(edu.wustl.query.util.global.Constants.ABSTRACT_QUERY, abstractQuery);
 		
+		preProcess(exportDataObject, sessionDataBean);
+		
+		ExportDataManager.export(sessionDataBean, exportDataObject);
+
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private SessionDataBean getSessionData(HttpServletRequest request)
+	{
+		Object obj = request.getSession().getAttribute(Constants.SESSION_DATA);
+
+		if (obj != null)
+		{
+			SessionDataBean sessionData = (SessionDataBean) obj;
+			return sessionData;
+		}
 		return null;
 	}
 
