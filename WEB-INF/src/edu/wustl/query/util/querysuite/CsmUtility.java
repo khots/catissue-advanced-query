04c67780f2473f6c998a948da31a61fe4055420c
chitra_garg <chitra_garg>
2009-03-23 08:29:23 +0000

    changes for 11720,11743
    SVN-Revision: 6302

--- WEB-INF/src/edu/wustl/query/util/querysuite/CsmUtility.java
+++ WEB-INF/src/edu/wustl/query/util/querysuite/CsmUtility.java
@@ -2,34 +2,197 @@ package edu.wustl.query.util.querysuite;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-
+import org.hibernate.Query;
+import org.hibernate.Session;
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
 import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
-import edu.wustl.common.security.PrivilegeCache;
-import edu.wustl.common.security.PrivilegeManager;
 import edu.wustl.common.security.PrivilegeUtility;
-import edu.wustl.common.util.Permissions;
-import edu.wustl.common.util.dbManager.HibernateUtility;
+import edu.wustl.common.util.dbManager.DBUtil;
 import edu.wustl.query.util.global.Constants;
 import gov.nih.nci.security.authorization.domainobjects.ProtectionElement;
 import gov.nih.nci.security.authorization.domainobjects.ProtectionGroup;
-import gov.nih.nci.security.authorization.domainobjects.User;
 import gov.nih.nci.security.dao.ProtectionElementSearchCriteria;
 import gov.nih.nci.security.exceptions.CSException;
 import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
 
 public class CsmUtility {
-	
-	private PrivilegeCache setPrivilegeCache(SessionDataBean sessionDataBean ) throws CSException
-	{
-		User user = new PrivilegeUtility().getUserProvisioningManager().getUser(sessionDataBean.getUserName());
-		sessionDataBean.setCsmUserId(user.getUserId().toString());
-		PrivilegeCache privilegeCache = PrivilegeManager.getInstance().getPrivilegeCache(sessionDataBean.getUserName());
-		return privilegeCache;
 
+	/**
+	 * @param authorizedQueryCollection= collection of my queries
+	 * @param sharedQueryCollection= collection of shared queries
+	 * @param sessionDataBean= details of logged in user
+	 * @throws CSException 
+	 * @throws CSObjectNotFoundException
+	 */
+	public void  checkExecuteQueryPrivilege (	
+			Collection<IParameterizedQuery> authorizedQueryCollection,
+			Collection<IParameterizedQuery> sharedQueryCollection,
+			SessionDataBean sessionDataBean ) throws CSException,
+			CSObjectNotFoundException
+	{
+		Collection<Long> parameterizedQueryCollection=getParameterizedQueryCollection();
+
+		Collection<Long> myQueriesIdList = new ArrayList<Long>();
+		Collection<Long> sharedQueriesIdList = new ArrayList<Long>();
+		for(Long parameterizedQueryId : parameterizedQueryCollection)
+		{
+				setSharedQueriesCollection(myQueriesIdList,sharedQueriesIdList,
+						parameterizedQueryId,sessionDataBean.getCsmUserId());
+				
+		}
+
+		authorizedQueryCollection=retrieveQueries(myQueriesIdList);
+		sharedQueryCollection=retrieveQueries(sharedQueriesIdList);
+	}
+	
+	/**
+	 * @param authorizedQueryCollection= collection of my queries
+	 * @param sharedQueryCollection= collection of shared queries
+	 * @param parameterizedQuery= id of parameterized query
+	 * @param csmUserId =csUser Id
+	 * @throws CSException
+	 * @throws CSObjectNotFoundException
+	 */
+	private void setSharedQueriesCollection(Collection<Long> authorizedQueryCollection,
+			Collection<Long> sharedQueryCollection,
+			Long parameterizedQuery,String csmUserId) throws CSException,
+			CSObjectNotFoundException
+	{
+		ProtectionElement pe = new ProtectionElement();
+
+		List<ProtectionElement> peList = new ArrayList<ProtectionElement>();
+		PrivilegeUtility privilegeUtility = new PrivilegeUtility();
+		pe.setObjectId(ParameterizedQuery.class.getName()+"_"+parameterizedQuery);
+		ProtectionElementSearchCriteria searchCriteria = new ProtectionElementSearchCriteria(pe);
+		peList = privilegeUtility.getUserProvisioningManager().getObjects(searchCriteria);
+		if (peList != null && !peList.isEmpty())
+		{
+			pe = peList.get(0);
+		}
+		
+		Set<ProtectionGroup> pgSet = privilegeUtility.getUserProvisioningManager().getProtectionGroups(pe.getProtectionElementId().toString());
+		
+		 populateSharedQueryCollection(authorizedQueryCollection,sharedQueryCollection, parameterizedQuery,
+				pgSet,csmUserId);
+		
+	}
+	
+	/**
+	 * @param authorizedQueryCollection
+	 * @param sharedQueryCollection
+	 * @param parameterizedQueryId
+	 * @param pgSet
+	 * @param csmUserId
+	 */
+	private void populateSharedQueryCollection(Collection<Long> authorizedQueryCollection,
+			Collection<Long> sharedQueryCollection,
+			Long parameterizedQueryId, Set<ProtectionGroup> pgSet,String csmUserId)
+	{
+		String userProtectionGroup=getUserProtectionGroup(csmUserId);
+		for(ProtectionGroup pg : pgSet)
+		{
+			if(pg.getProtectionGroupName().equals(Constants.PUBLIC_QUERY_PROTECTION_GROUP))
+			{
+				sharedQueryCollection.add(parameterizedQueryId);
+			}
+			if(pg.getProtectionGroupName().equals(userProtectionGroup))
+			{
+				authorizedQueryCollection.add(parameterizedQueryId);
+			}
+		}
+
+	}
+	/**
+	 * @param csmUserId
+	 * @return
+	 */
+	public String getUserProtectionGroup(String csmUserId)
+	{
+		return "User_" + csmUserId;
+	}
+	/**
+	 * @return
+	 */
+	public static Collection<Long> getParameterizedQueryCollection()
+	{
+		Session session1 = null;
+		try {
+			session1 = DBUtil.getCleanSession();
+			Query query = null;
+			
+			query = session1.createQuery("select id from " + IParameterizedQuery.class.getName()
+					
+					 );
+
+		
+			return query.list();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			session1.close();
+		}
+		return null;
+		
+	}
+	/**
+	 * @param ids
+	 * @return
+	 */
+	public static Collection<IParameterizedQuery> retrieveQueries(Collection<Long>  ids)
+	{
+		StringBuffer objectToRead = generateListOfIds(ids);
+		
+		Session session1 = null;
+		
+		try {
+			session1 = DBUtil.getCleanSession();
+			if(!objectToRead.toString().equals("()"))
+			{
+				
+				Query query = null;
+				
+				query = session1.createQuery("from " + IParameterizedQuery.class.getName()  + 
+						" query where  query.id in  "+objectToRead
+						 );
+	
+			
+				return query.list();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			session1.close();
+		}
+		return null;
+	}
+	private static StringBuffer generateListOfIds(Collection<Long> ids) {
+		StringBuffer objectToRead = new StringBuffer();
+		objectToRead.append("(");
+		Iterator<Long> idIter=ids.iterator();
+
+		for (int j = 0; j < ids.size(); j++) 
+		{
+
+			objectToRead.append(idIter.next());
+
+			if(j<ids.size()-1)
+			{
+				objectToRead.append(",");
+			}
+		}
+
+		objectToRead.append(")");
+		return objectToRead;
 	}
 	
 	/**
@@ -64,81 +227,6 @@ public class CsmUtility {
 			}
 		}
 		return sharedQuery;
-		
-	}
-	public void  checkExecuteQueryPrivilege(
-			Collection<IParameterizedQuery> authorizedQueryCollection,
-			Collection<IParameterizedQuery> sharedQueryCollection,
-			SessionDataBean sessionDataBean ) throws CSException,
-			CSObjectNotFoundException
-	{
-		Collection<IParameterizedQuery> parameterizedQueryCollection=getParameterizedQueryCollection();
-		
-		PrivilegeCache privilegeCache =setPrivilegeCache(sessionDataBean);
-		for(IParameterizedQuery parameterizedQuery : parameterizedQueryCollection)
-		{
-			String objectId = ((ParameterizedQuery)parameterizedQuery).getObjectId();
-			if(privilegeCache.hasPrivilege(objectId, Permissions.EXECUTE_QUERY))
-			{
-				boolean sharedQuery = setSharedQueriesCollection(sharedQueryCollection,
-						parameterizedQuery, objectId,sessionDataBean.getCsmUserId());
-				if(!sharedQuery)
-				{
-					authorizedQueryCollection.add(parameterizedQuery);
-				}
-			}
-		}
-	}
-	
-	private boolean setSharedQueriesCollection(
-			Collection<IParameterizedQuery> sharedQueryCollection,
-			IParameterizedQuery parameterizedQuery, String objectId,String csmUserId) throws CSException,
-			CSObjectNotFoundException
-	{
-		ProtectionElement pe = new ProtectionElement();
-
-		List<ProtectionElement> peList = new ArrayList<ProtectionElement>();
-		PrivilegeUtility privilegeUtility = new PrivilegeUtility();
-		pe.setObjectId(objectId);
-		ProtectionElementSearchCriteria searchCriteria = new ProtectionElementSearchCriteria(pe);
-		peList = privilegeUtility.getUserProvisioningManager().getObjects(searchCriteria);
-		boolean sharedQuery = false;
-		if (peList != null && !peList.isEmpty())
-		{
-			pe = peList.get(0);
-		}
-		
-		Set<ProtectionGroup> pgSet = privilegeUtility.getUserProvisioningManager().getProtectionGroups(pe.getProtectionElementId().toString());
-		
-		sharedQuery = populateSharedQueryCollection(sharedQueryCollection, parameterizedQuery,
-				pgSet,csmUserId);
-		return sharedQuery;
-	}
-	
-	private boolean populateSharedQueryCollection(
-			Collection<IParameterizedQuery> sharedQueryCollection,
-			IParameterizedQuery parameterizedQuery, Set<ProtectionGroup> pgSet,String csmUserId)
-	{
-		boolean sharedQuery=false;
-		for(ProtectionGroup pg : pgSet)
-		{
-			if(pg.getProtectionGroupName().equals(Constants.PUBLIC_QUERY_PROTECTION_GROUP))
-			{
-				sharedQueryCollection.add(parameterizedQuery);
-				sharedQuery = true;
-			}
-		}
-		return sharedQuery;
-	}
-	public String getUserProtectionGroup(String csmUserId)
-	{
-		return "User_" + csmUserId;
-	}
-	public static Collection getParameterizedQueryCollection()
-	{
-
-		return	HibernateUtility
-		.executeHQL(HibernateUtility.GET_PARAMETERIZED_QUERIES_DETAILS);
 		
 	}
 }
