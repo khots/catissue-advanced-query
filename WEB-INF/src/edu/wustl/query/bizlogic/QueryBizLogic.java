3b03a11979f700b2a9aaa20b9808cfa141597c14
pavan_kalantri <pavan_kalantri>
2009-03-23 05:13:18 +0000

    added a method to check weather the given query is shared or not & update the Query object
    SVN-Revision: 6289

--- WEB-INF/src/edu/wustl/query/bizlogic/QueryBizLogic.java
+++ WEB-INF/src/edu/wustl/query/bizlogic/QueryBizLogic.java
@@ -8,9 +8,11 @@ import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.bizlogic.DefaultBizLogic;
 import edu.wustl.common.dao.DAO;
 import edu.wustl.common.exception.BizLogicException;
+import edu.wustl.common.query.authoriztion.SavedQueryAuthorization;
 import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
 import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
 import edu.wustl.common.util.dbManager.DAOException;
+import edu.wustl.query.util.global.Constants;
 
 /**
  * @author chetan_patil
@@ -19,11 +21,23 @@ import edu.wustl.common.util.dbManager.DAOException;
 public class QueryBizLogic extends DefaultBizLogic
 {
 
+	/* (non-Javadoc)
+	 * @see edu.wustl.common.bizlogic.DefaultBizLogic#insert(java.lang.Object, edu.wustl.common.dao.DAO, edu.wustl.common.beans.SessionDataBean)
+	 */
 	protected void insert(Object obj, DAO dao, SessionDataBean sessionDataBean)
 	throws DAOException, UserNotAuthorizedException
 	{
 		super.insert(obj, dao, sessionDataBean);
 	}
+	/**
+	 * It will insert the Query object in the database.
+	 * @param query
+	 * @param sessionDataBean
+	 * @param shareQuery
+	 * @throws DAOException
+	 * @throws UserNotAuthorizedException
+	 * @throws BizLogicException
+	 */
 	public void insertSavedQueries(IParameterizedQuery query,SessionDataBean sessionDataBean, boolean shareQuery)throws DAOException, UserNotAuthorizedException, BizLogicException
 	{
 		insert(query,edu.wustl.common.util.global.Constants.HIBERNATE_DAO);
@@ -44,5 +58,40 @@ public class QueryBizLogic extends DefaultBizLogic
 //		SavedQueryAuthorization savedQuery = new SavedQueryAuthorization();
 //		savedQuery.authenticate(protectionObjects,user.getUserId().toString(),shareQuery,user);
 	}
-
+	/**
+	 * It will update the query object in the dataBase
+	 * @param query
+	 * @param sessionDataBean
+	 * @param shareQuery
+	 * @throws DAOException
+	 * @throws UserNotAuthorizedException
+	 * @throws BizLogicException
+	 */
+	public void updateSavedQueries(IParameterizedQuery query,SessionDataBean sessionDataBean, boolean shareQuery)throws DAOException, UserNotAuthorizedException, BizLogicException
+	{
+		super.update(query, Constants.HIBERNATE_DAO);
+	}
+	
+	/**
+	 * It will check weather the given query is shared by the user or not.
+	 * @param query which is to be checked.
+	 * @return true if query is shared else false .
+	 * @throws BizLogicException
+	 */
+	public boolean isSharedQuery(IParameterizedQuery query)throws BizLogicException
+	{
+		SavedQueryAuthorization savedQuery = new SavedQueryAuthorization();
+		boolean isShared=false;
+		try
+		{
+			isShared = savedQuery.checkIsSharedQuery(query);
+			
+		}
+		catch (DAOException e) 
+		{
+			throw new BizLogicException(e);
+		}
+		
+		return isShared;
+	}
 }
