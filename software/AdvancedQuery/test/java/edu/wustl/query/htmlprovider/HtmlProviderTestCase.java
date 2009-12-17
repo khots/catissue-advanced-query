1d44606737c5090e7d145b14b110f6172ca30bc0
pooja_tavase <pooja_tavase>
2009-12-17 11:35:42 +0000

    HtmlProvider test cases.
    SVN-Revision: 10202

---
+++ software/AdvancedQuery/test/java/edu/wustl/query/htmlprovider/HtmlProviderTestCase.java
@@ -0,0 +1,32 @@
+package edu.wustl.query.htmlprovider;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.cab2b.server.cache.EntityCache;
+import edu.wustl.query.generator.GenericQueryGeneratorMock;
+import edu.wustl.query.util.querysuite.EntityCacheFactory;
+import junit.framework.TestCase;
+
+public class HtmlProviderTestCase extends TestCase
+{
+	/**
+	 * Test the HTML generated.
+	 */
+	public void testGenerateHtml()
+	{
+		try
+		{
+			HtmlProvider htmlProvider = new HtmlProvider(null);
+			EntityCache cache = EntityCacheFactory.getInstance();
+            EntityInterface participantEntity = GenericQueryGeneratorMock.createEntity("Participant");
+            participantEntity = GenericQueryGeneratorMock.getEntity(cache, participantEntity);
+			htmlProvider.generateHTML(participantEntity,null);
+			assertTrue("Html generated successfully for Demographics.",true);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+
+	}
+}
