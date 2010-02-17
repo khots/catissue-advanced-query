47404b13426b487c1a62e04bb42c42ab85a1bac6
pooja_tavase <pooja_tavase>
2010-02-17 14:03:20 +0000

    Added/Updated test cases to increase the code coverage.
    SVN-Revision: 10318

--- software/AdvancedQuery/test/java/edu/wustl/query/htmlprovider/GenerateHtmlTestCase.java
+++ software/AdvancedQuery/test/java/edu/wustl/query/htmlprovider/GenerateHtmlTestCase.java
@@ -3,6 +3,16 @@ package edu.wustl.query.htmlprovider;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.cab2b.server.cache.EntityCache;
+import edu.wustl.common.querysuite.queryobject.ICondition;
+import edu.wustl.common.querysuite.queryobject.IParameter;
+import edu.wustl.common.querysuite.queryobject.RelationalOperator;
+import edu.wustl.query.generator.GenericQueryGeneratorMock;
+import edu.wustl.query.util.global.AQConstants;
+import edu.wustl.query.util.querysuite.EntityCacheFactory;
+
 import junit.framework.TestCase;
 
 public class GenerateHtmlTestCase extends TestCase
@@ -15,6 +25,12 @@ public class GenerateHtmlTestCase extends TestCase
 		GenerateHtml.generateHTMLForRadioButton("isAvailable13761", values);
 		values = new ArrayList<String>();
 		values.add("false");
+		GenerateHtml.generateHTMLForRadioButton("isAvailable13761", values);
+		values = new ArrayList<String>();
+		values.add("true");
+		GenerateHtml.generateHTMLForRadioButton("isAvailable13761", values);
+		values = new ArrayList<String>();
+		values.add("other");
 		GenerateHtml.generateHTMLForRadioButton("isAvailable13761", values);
 	}
 
@@ -36,5 +52,82 @@ public class GenerateHtmlTestCase extends TestCase
 
 		GenerateHtml.generatePreHtml(attributeCollection, "edu.wustl.clinportal.domain.Participant", false);
 		GenerateHtml.generatePreHtml(attributeCollection, "edu.wustl.clinportal.domain.Participant", true);
+	}
+
+	public void testGetHtmlAddEditPage()
+	{
+		GenerateHtml.getHtmlAddEditPage(AQConstants.ADD_EDIT_PAGE, new StringBuffer());
+	}
+
+	public void testGetBoldLabel()
+	{
+		GenerateHtml.getBoldLabel("Label");
+	}
+
+	public void testGetAlternateCss()
+	{
+		GenerateHtml.getAlternateCss(new StringBuffer(), false, "isAvailable13761");
+		GenerateHtml.getAlternateCss(new StringBuffer(), true, "isAvailable13761");
+	}
+
+	public void testCheckBetweenOperator()
+	{
+		GenerateHtml.checkBetweenOperator("Between");
+	}
+
+	public void testGenerateHTMLForOperator()
+	{
+		List<String> operatorList = new ArrayList<String>();
+		operatorList.add("Equals");
+		operatorList.add("Not Equals");
+		operatorList.add("Less than");
+		operatorList.add("Less than or Equal to");
+		operatorList.add("Greater than");
+		operatorList.add("Greater than or Equal to");
+		GenerateHtml.generateHTMLForOperator("comp0", "Not Equals", operatorList, true);
+	}
+
+	public void testGenerateHTMLForTextBox()
+	{
+		AttributeDetails attributeDetails = new AttributeDetails();
+		attributeDetails.setSelectedOperator(RelationalOperator.IsNotNull.toString());
+		List<IParameter<?>> parameterList = attributeDetails.getParameterList();
+		EntityCache cache = EntityCacheFactory.getInstance();
+        EntityInterface participantEntity = GenericQueryGeneratorMock.createEntity("Participant");
+        participantEntity = GenericQueryGeneratorMock.getEntity(cache, participantEntity);
+		AttributeInterface attribute = GenericQueryGeneratorMock.findAttribute(participantEntity, "birthDate");
+		ICondition participantCondition = GenericQueryGeneratorMock.createParticipantCondition(participantEntity);
+		List<ICondition> conditions = new ArrayList<ICondition>();
+		conditions.add(participantCondition);
+		attributeDetails.setAttrName(attribute.getName());
+		attributeDetails.setConditions(conditions);
+		attributeDetails.setDataType(attribute.getDataType());
+		ICondition condition  = null;
+		attributeDetails.setAttributeNameConditionMap(HtmlUtility.getMapOfConditions(conditions));
+		if(attributeDetails.getAttributeNameConditionMap()!=null)
+		{
+			condition = attributeDetails.getAttributeNameConditionMap().
+							get(attributeDetails.getAttrName());
+		}
+		attributeDetails.setEditValues(null);
+		attributeDetails.setSelectedOperator(null);
+		if(condition != null)
+		{
+			attributeDetails.setEditValues(condition.getValues());
+			attributeDetails.setSelectedOperator
+				(condition.getRelationalOperator().getStringRepresentation());
+		}
+		List<String> operatorList = new ArrayList<String>();
+		operatorList.add("Between");
+		operatorList.add("Greater than");
+		operatorList.add("Equals");
+		operatorList.add("Greater than or Equal to");
+		operatorList.add("Is Not Null");
+		operatorList.add("Is Null");
+		operatorList.add("Not Equals");
+		operatorList.add("Less than or Equal to");
+		attributeDetails.setOperatorsList(operatorList);
+		GenerateHtml.generateHTMLForTextBox("birthDate94", attributeDetails);
+		GenerateHtml.generateHTMLForOperators("birthDate94", true, attributeDetails);
 	}
 }
