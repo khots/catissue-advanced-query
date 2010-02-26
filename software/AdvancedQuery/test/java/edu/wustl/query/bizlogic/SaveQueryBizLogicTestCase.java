0d2f93fda48231ca1fd6b6e0b20212f767872f98
nitesh_marwaha <nitesh_marwaha>
2010-02-26 08:19:37 +0000

    test cases added for saveQueryBizlogic
    SVN-Revision: 10344

--- software/AdvancedQuery/test/java/edu/wustl/query/bizlogic/SaveQueryBizLogicTestCase.java
+++ software/AdvancedQuery/test/java/edu/wustl/query/bizlogic/SaveQueryBizLogicTestCase.java
@@ -1,50 +1,74 @@
 package edu.wustl.query.bizlogic;
 
+import junit.framework.TestCase;
+import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
 import edu.wustl.common.querysuite.queryobject.IQuery;
+import edu.wustl.query.beans.SharedQueryBean;
 import edu.wustl.query.generator.GenericQueryGeneratorMock;
-import junit.framework.TestCase;
-
 public class SaveQueryBizLogicTestCase extends TestCase
 {
-	public void populateParameterizedQueryData()
+
+	private SessionDataBean getSession()
 	{
-		IQuery query = GenericQueryGeneratorMock.creatParticipantQuery();
+		SessionDataBean sessionDataBean = new SessionDataBean();
+		sessionDataBean.setIpAddress("");
+		sessionDataBean.setUserId(1l);
+		sessionDataBean.setUserName("admin@admin.com");
+
+		return sessionDataBean;
+	}
+	public void testSaveQuery()
+	{
+		IQuery query= GenericQueryGeneratorMock.creatParticipantQuery();
 		SaveQueryBizLogic bizLogic = new SaveQueryBizLogic();
-		IParameterizedQuery originalQuery = bizLogic.populateParameterizedQueryData(query);
-	}
+		IParameterizedQuery parameterizedQuery = bizLogic.populateParameterizedQueryData(query);
 
-	public void testGetQuery()
-	{
 		try
 		{
-			IQuery query = new SaveQueryBizLogic().getQuery(Long.valueOf(1));
+			SharedQueryBean queryBean = new SharedQueryBean();
+			queryBean.setShareTo("none");
+			bizLogic.saveQuery(parameterizedQuery, getSession(), null);
+			Long queryId = parameterizedQuery.getId();
+			IQuery retrievedQuery = new SaveQueryBizLogic().getQuery(queryId);
+			assertNotNull(query);
+
 		}
 		catch (BizLogicException e)
 		{
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+//	public void testPopulateParameterizedQueryData()
+//	{
+//		IQuery query = GenericQueryGeneratorMock.creatParticipantQuery();
+//		SaveQueryBizLogic bizLogic = new SaveQueryBizLogic();
+//		IParameterizedQuery originalQuery = bizLogic.populateParameterizedQueryData(query);
+//	}
 
-	public void testGetUserById()
-	{
-		try
-		{
-			new SaveQueryBizLogic().getUserById("1");
-		}
-		catch (BizLogicException e)
-		{
-			e.printStackTrace();
-		}
-	}
+
+//	public void testGetQuery()
+//	{
+//		try
+//		{
+//			IQuery query = new SaveQueryBizLogic().getQuery(Long.valueOf(1));
+//		}
+//		catch (BizLogicException e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+
 
 	public void testGetSharingDetailsBean()
 	{
 		IQuery query = GenericQueryGeneratorMock.creatParticipantQuery();
 		try
 		{
-			new SaveQueryBizLogic().getSharingDetailsBean(query);
+			SharedQueryBean queryBean= new SaveQueryBizLogic().getSharingDetailsBean(query);
+			assertNotNull(queryBean);
 		}
 		catch (BizLogicException e)
 		{
