2b638b1bf805c7c255e908a88cf4a07f2360facd
Atul Kaushal <atul.kaushal@krishagni.com>
2012-10-15 13:17:36 +0000

    Changes made for security fixes.
    SVN-Revision: 10858

--- software/AdvancedQuery/src/java/edu/wustl/query/action/FetchQueryAction.java
+++ software/AdvancedQuery/src/java/edu/wustl/query/action/FetchQueryAction.java
@@ -20,6 +20,7 @@ import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.querysuite.queryobject.ICustomFormula;
 import edu.wustl.common.querysuite.queryobject.IParameterizedQuery;
 import edu.wustl.common.querysuite.queryobject.impl.ParameterizedQuery;
+import edu.wustl.common.util.global.Constants;
 import edu.wustl.query.actionForm.SaveQueryForm;
 import edu.wustl.query.bizlogic.BizLogicFactory;
 import edu.wustl.query.htmlprovider.SavedQueryHtmlProvider;
@@ -44,17 +45,29 @@ public class FetchQueryAction extends Action
 			HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
 		String target = AQConstants.FAILURE;
-		SaveQueryForm saveQueryForm = (SaveQueryForm) actionForm;
-		request.setAttribute(AQConstants.HIDE_TREE_CHECK_VALUE, request.getSession().getAttribute(AQConstants.TREE_CHECK_VALUE));
-		if (request.getAttribute("queryId") == null)
+		ActionForward actionfwd;
+		if ( !isTokenValid(request) ) 
 		{
-			target = fetchQueryDetails(request, target, saveQueryForm);
+			actionfwd = actionMapping.findForward(Constants.FAILURE);
+			ActionErrors actionErrors = new ActionErrors();
+			ActionError actionError = new ActionError("errors.item","Invalid request for add/edit operaton");
+			actionErrors.add(ActionErrors.GLOBAL_ERROR, actionError);
+			saveErrors(request, actionErrors);
 		}
 		else
 		{
-			String htmlContent = saveQueryForm.getQueryString();
-			request.setAttribute(AQConstants.HTML_CONTENTS, htmlContent);
-			target = AQConstants.SUCCESS;
+			SaveQueryForm saveQueryForm = (SaveQueryForm) actionForm;
+			request.setAttribute(AQConstants.HIDE_TREE_CHECK_VALUE, request.getSession().getAttribute(AQConstants.TREE_CHECK_VALUE));
+			if (request.getAttribute("queryId") == null)
+			{
+				target = fetchQueryDetails(request, target, saveQueryForm);
+			}
+			else
+			{
+				String htmlContent = saveQueryForm.getQueryString();
+				request.setAttribute(AQConstants.HTML_CONTENTS, htmlContent);
+				target = AQConstants.SUCCESS;
+			}
 		}
 		return actionMapping.findForward(target);
 	}
